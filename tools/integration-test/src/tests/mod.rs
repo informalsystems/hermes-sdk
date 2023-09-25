05026f37fe6a8fd73d7106f5577735bb916d836a
Soares Chen <soares.chen@maybevoid.com>
2023-09-25 15:07:06 +0200

    Fix CI

--- tools/integration-test/src/tests/mod.rs
+++ tools/integration-test/src/tests/mod.rs
@@ -2,6 +2,8 @@ pub mod connection;
 pub mod context;
 pub mod filter;
 pub mod packet_clear;
-pub mod solomachine;
 pub mod timeout_transfer;
 pub mod transfer;
+
+#[cfg(feature = "solomachine")]
+pub mod solomachine;
