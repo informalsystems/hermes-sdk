490009613d75c7a71e2b5e2815402d68e1e8e095
Soares Chen <soares.chen@maybevoid.com>
2023-09-25 19:47:06 +0200

    Add proc macro dependencies

--- crates/cgp-macros/src/lib.rs
+++ crates/cgp-macros/src/lib.rs
@@ -3,8 +3,12 @@
 extern crate proc_macro;
 
 use proc_macro::TokenStream;
+use quote::ToTokens;
+use syn::{parse_macro_input, ItemTrait};
 
 #[proc_macro_attribute]
 pub fn derive_component(_attr: TokenStream, item: TokenStream) -> TokenStream {
-    item
+    let input = parse_macro_input!(item as ItemTrait);
+
+    input.to_token_stream().into()
 }
