64e7b33f331828810378c2e67de2ed68d6fd96c8
Soares Chen <soares.chen@maybevoid.com>
2023-09-27 21:08:00 +0200

    Move cgp crates to https://github.com/informalsystems/cgp

--- crates/cgp-core/src/traits/mod.rs
+++
@@ -1,9 +0,0 @@
-pub mod delegate_component;
-pub mod error;
-pub mod has_components;
-pub mod sync;
-
-pub use delegate_component::DelegateComponent;
-pub use error::{CanRaiseError, HasErrorType};
-pub use has_components::HasComponents;
-pub use sync::Async;
