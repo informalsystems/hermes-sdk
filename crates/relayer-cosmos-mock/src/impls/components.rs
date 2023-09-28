ba3d3cac2d044ce4d1c0fd0d168046719d332d35
Soares Chen <soares.chen@maybevoid.com>
2023-09-28 14:58:07 +0200

    Implement mock cosmos chain (#40)
    * feat: implement mock cosmos chain
    
    * fix: clean up relay impls
    
    * imp: chain handler refinement
    
    * feat: add update client test
    
    * fix: patch tendermint testgen
    
    * imp: use create client traits
    
    * imp: separate mock basecoin from chain context
    
    * imp: separate chain handles from endpoints
    
    * fix: update cargo.lock
    
    * imp: better break down of methods for basecoin
    
    * imp: use random generated app_hash
    
    * fix: apply suggestions from code review
    
    Co-authored-by: Sean Chen <seanchen11235@gmail.com>
    Signed-off-by: Farhad Shabani <Farhad.Shabani@gmail.com>
    
    * imp: add more docstings + change the naming of BasecoinHandler to BasecoinRunner
    
    * nit: drop redundant state declaration
    
    * fix: add validators field to MockBasecoin
    
    * imp: rename connect method to sync
    
    * imp: use TokioRuntimeContext instead
    
    * imp: incorporate MockCosmosBuilder
    
    * Add docstring for `MockCosmosRelay`
    
    Co-authored-by: Sean Chen <seanchen11235@gmail.com>
    Signed-off-by: Farhad Shabani <Farhad.Shabani@gmail.com>
    
    * imp: some clean-ups
    
    * nit: cargo fmt
    
    * fix: implement missed traits for chain and component
    
    * fix: test_update_client should query dst_chain to see client_state updated
    
    * fix: use store's root_hash as app_hash for headers
    
    * deps: bump ibc, ibc-proto, tendermint, basecoin
    
    * Merge with main
    
    * Run cargo +nightly fmt
    
    * Fix merge error
    
    ---------
    
    Signed-off-by: Farhad Shabani <Farhad.Shabani@gmail.com>
    Co-authored-by: Farhad Shabani <farhad.shabani@gmail.com>
    Co-authored-by: Sean Chen <seanchen11235@gmail.com>

---
+++ crates/relayer-cosmos-mock/src/impls/components.rs
@@ -0,0 +1,60 @@
+use ibc_relayer_components::relay::components::message_senders::chain_sender::SendIbcMessagesToChain;
+use ibc_relayer_components::relay::components::message_senders::update_client::SendIbcMessagesWithUpdateClient;
+use ibc_relayer_components::relay::components::packet_filters::allow_all::AllowAll;
+use ibc_relayer_components::relay::components::packet_relayers::general::full_relay::FullCycleRelayer;
+use ibc_relayer_components::relay::components::update_client::skip::SkipUpdateClient;
+use ibc_relayer_components::relay::components::update_client::wait::WaitUpdateClient;
+use ibc_relayer_components::relay::components::packet_relayers::ack::base_ack_packet::BaseAckPacketRelayer;
+use ibc_relayer_components::relay::components::packet_relayers::receive::base_receive_packet::BaseReceivePacketRelayer;
+use ibc_relayer_components::relay::components::packet_relayers::receive::skip_received_packet::SkipReceivedPacketRelayer;
+use ibc_relayer_components::relay::components::packet_relayers::timeout_unordered::timeout_unordered_packet::BaseTimeoutUnorderedPacketRelayer;
+use ibc_relayer_components::relay::traits::components::ibc_message_sender::{IbcMessageSenderComponent, MainSink};
+use ibc_relayer_components::relay::traits::components::packet_filter::PacketFilterComponent;
+use ibc_relayer_components::relay::traits::components::packet_relayer::PacketRelayerComponent;
+use ibc_relayer_components::relay::traits::components::packet_relayers::ack_packet::AckPacketRelayerComponent;
+use ibc_relayer_components::relay::traits::components::packet_relayers::receive_packet::ReceivePacketRelayerComponnent;
+use ibc_relayer_components::relay::traits::components::packet_relayers::timeout_unordered_packet::TimeoutUnorderedPacketRelayerComponent;
+use ibc_relayer_components::relay::traits::components::update_client_message_builder::UpdateClientMessageBuilderComponent;
+use cgp_core::delegate_component;
+
+use crate::impls::relay::MockCosmosBuildUpdateClientMessage;
+
+pub struct MockCosmosComponents;
+
+delegate_component!(
+    PacketRelayerComponent,
+    MockCosmosComponents,
+    FullCycleRelayer,
+);
+
+delegate_component!(
+    UpdateClientMessageBuilderComponent,
+    MockCosmosComponents,
+    SkipUpdateClient<WaitUpdateClient<MockCosmosBuildUpdateClientMessage>>,
+);
+
+delegate_component!(
+    IbcMessageSenderComponent<MainSink>,
+    MockCosmosComponents,
+    SendIbcMessagesWithUpdateClient<SendIbcMessagesToChain>,
+);
+
+delegate_component!(
+    ReceivePacketRelayerComponnent,
+    MockCosmosComponents,
+    SkipReceivedPacketRelayer<BaseReceivePacketRelayer>,
+);
+
+delegate_component!(
+    AckPacketRelayerComponent,
+    MockCosmosComponents,
+    BaseAckPacketRelayer,
+);
+
+delegate_component!(
+    TimeoutUnorderedPacketRelayerComponent,
+    MockCosmosComponents,
+    BaseTimeoutUnorderedPacketRelayer,
+);
+
+delegate_component!(PacketFilterComponent, MockCosmosComponents, AllowAll);
---
+++ crates/relayer-cosmos-mock/src/impls/relay.rs
@@ -0,0 +1,228 @@
+use alloc::boxed::Box;
+use alloc::vec::Vec;
+
+use async_trait::async_trait;
+use cgp_core::traits::{DelegateComponent, HasComponents, HasErrorType};
+use ibc::clients::ics07_tendermint::client_type;
+use ibc::clients::ics07_tendermint::header::Header;
+use ibc::core::ics02_client::msgs::update_client::MsgUpdateClient;
+use ibc::core::ics04_channel::packet::Packet;
+use ibc::core::ics24_host::identifier::ClientId;
+use ibc::core::{Msg, ValidationContext};
+use ibc::{Any, Height};
+use ibc_relayer_components::components::default::closures::relay::packet_relayer::CanUseDefaultPacketRelayer;
+use ibc_relayer_components::components::default::relay::DefaultRelayComponents;
+use ibc_relayer_components::logger::traits::has_logger::{HasLogger, HasLoggerType};
+use ibc_relayer_components::relay::traits::chains::HasRelayChains;
+use ibc_relayer_components::relay::traits::components::update_client_message_builder::UpdateClientMessageBuilder;
+use ibc_relayer_components::relay::traits::packet_lock::HasPacketLock;
+use ibc_relayer_components::relay::traits::target::{DestinationTarget, SourceTarget};
+use ibc_relayer_components::runtime::traits::runtime::HasRuntime;
+use ibc_relayer_runtime::types::error::Error as TokioError;
+use ibc_relayer_runtime::types::log::logger::TracingLogger;
+use ibc_relayer_runtime::types::runtime::TokioRuntimeContext;
+
+use crate::contexts::chain::MockCosmosContext;
+use crate::contexts::relay::MockCosmosRelay;
+use crate::impls::components::MockCosmosComponents;
+use crate::traits::endpoint::BasecoinEndpoint;
+use crate::types::error::Error;
+use crate::util::dummy::dummy_signer;
+
+impl<Name, SrcChain, DstChain> DelegateComponent<Name> for MockCosmosRelay<SrcChain, DstChain>
+where
+    SrcChain: BasecoinEndpoint,
+    DstChain: BasecoinEndpoint,
+{
+    type Delegate = DefaultRelayComponents<MockCosmosComponents>;
+}
+
+impl<SrcChain, DstChain> HasComponents for MockCosmosRelay<SrcChain, DstChain>
+where
+    SrcChain: BasecoinEndpoint,
+    DstChain: BasecoinEndpoint,
+{
+    type Components = DefaultRelayComponents<MockCosmosComponents>;
+}
+
+impl<SrcChain, DstChain> CanUseDefaultPacketRelayer for MockCosmosRelay<SrcChain, DstChain>
+where
+    SrcChain: BasecoinEndpoint,
+    DstChain: BasecoinEndpoint,
+{
+}
+
+impl<SrcChain, DstChain> HasErrorType for MockCosmosRelay<SrcChain, DstChain>
+where
+    SrcChain: BasecoinEndpoint,
+    DstChain: BasecoinEndpoint,
+{
+    type Error = Error;
+}
+
+impl<SrcChain, DstChain> HasRuntime for MockCosmosRelay<SrcChain, DstChain>
+where
+    SrcChain: BasecoinEndpoint,
+    DstChain: BasecoinEndpoint,
+{
+    type Runtime = TokioRuntimeContext;
+
+    fn runtime(&self) -> &Self::Runtime {
+        &self.runtime
+    }
+
+    fn runtime_error(e: TokioError) -> Error {
+        Error::source(e)
+    }
+}
+
+impl<SrcChain, DstChain> HasLoggerType for MockCosmosRelay<SrcChain, DstChain>
+where
+    SrcChain: BasecoinEndpoint,
+    DstChain: BasecoinEndpoint,
+{
+    type Logger = TracingLogger;
+}
+
+impl<SrcChain, DstChain> HasLogger for MockCosmosRelay<SrcChain, DstChain>
+where
+    SrcChain: BasecoinEndpoint,
+    DstChain: BasecoinEndpoint,
+{
+    fn logger(&self) -> &TracingLogger {
+        &TracingLogger
+    }
+}
+
+impl<SrcChain, DstChain> HasRelayChains for MockCosmosRelay<SrcChain, DstChain>
+where
+    SrcChain: BasecoinEndpoint,
+    DstChain: BasecoinEndpoint,
+{
+    type Packet = Packet;
+
+    type SrcChain = MockCosmosContext<SrcChain>;
+
+    type DstChain = MockCosmosContext<DstChain>;
+
+    fn src_chain_error(e: Error) -> Self::Error {
+        e
+    }
+
+    fn dst_chain_error(e: Error) -> Self::Error {
+        e
+    }
+
+    fn src_chain(&self) -> &MockCosmosContext<SrcChain> {
+        &self.src_chain
+    }
+
+    fn dst_chain(&self) -> &MockCosmosContext<DstChain> {
+        &self.dst_chain
+    }
+
+    fn src_client_id(&self) -> &ClientId {
+        self.src_client_id()
+    }
+
+    fn dst_client_id(&self) -> &ClientId {
+        self.dst_client_id()
+    }
+}
+
+pub struct MockCosmosBuildUpdateClientMessage;
+
+#[async_trait]
+impl<SrcChain, DstChain>
+    UpdateClientMessageBuilder<MockCosmosRelay<SrcChain, DstChain>, SourceTarget>
+    for MockCosmosBuildUpdateClientMessage
+where
+    SrcChain: BasecoinEndpoint,
+    DstChain: BasecoinEndpoint,
+{
+    async fn build_update_client_messages(
+        context: &MockCosmosRelay<SrcChain, DstChain>,
+        _target: SourceTarget,
+        height: &Height,
+    ) -> Result<Vec<Any>, Error> {
+        let client_counter = context.dst_chain().ibc_context().client_counter()?;
+
+        let client_id = ClientId::new(client_type(), client_counter)?;
+
+        let client_state = context
+            .dst_chain()
+            .ibc_context()
+            .client_state(&ClientId::default())?;
+
+        let light_block = context.src_chain().get_light_block(height)?;
+
+        let header = Header {
+            signed_header: light_block.signed_header,
+            validator_set: light_block.validators,
+            trusted_height: client_state.latest_height,
+            trusted_next_validator_set: light_block.next_validators,
+        };
+
+        let msg_update_client = MsgUpdateClient {
+            client_id,
+            client_message: header.into(),
+            signer: dummy_signer(),
+        };
+
+        Ok(vec![msg_update_client.to_any()])
+    }
+}
+
+#[async_trait]
+impl<SrcChain, DstChain>
+    UpdateClientMessageBuilder<MockCosmosRelay<SrcChain, DstChain>, DestinationTarget>
+    for MockCosmosBuildUpdateClientMessage
+where
+    SrcChain: BasecoinEndpoint,
+    DstChain: BasecoinEndpoint,
+{
+    async fn build_update_client_messages(
+        context: &MockCosmosRelay<SrcChain, DstChain>,
+        _target: DestinationTarget,
+        height: &Height,
+    ) -> Result<Vec<Any>, Error> {
+        let client_counter = context.src_chain().ibc_context().client_counter()?;
+
+        let client_id = ClientId::new(client_type(), client_counter)?;
+
+        let client_state = context
+            .src_chain()
+            .ibc_context()
+            .client_state(&ClientId::default())?;
+
+        let light_block = context.dst_chain().get_light_block(height)?;
+
+        let header = Header {
+            signed_header: light_block.signed_header,
+            validator_set: light_block.validators,
+            trusted_height: client_state.latest_height,
+            trusted_next_validator_set: light_block.next_validators,
+        };
+
+        let msg_update_client = MsgUpdateClient {
+            client_id,
+            client_message: header.into(),
+            signer: dummy_signer(),
+        };
+
+        Ok(vec![msg_update_client.to_any()])
+    }
+}
+
+#[async_trait]
+impl<SrcChain, DstChain> HasPacketLock for MockCosmosRelay<SrcChain, DstChain>
+where
+    SrcChain: BasecoinEndpoint,
+    DstChain: BasecoinEndpoint,
+{
+    type PacketLock<'a> = ();
+
+    async fn try_acquire_packet_lock<'a>(&'a self, _packet: &'a Packet) -> Option<()> {
+        Some(())
+    }
+}
