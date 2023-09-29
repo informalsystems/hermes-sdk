3d167bb0ceb5de5c2005719ccd27c118c8399e7e
Soares Chen <soares.chen@maybevoid.com>
2023-09-29 07:33:02 +0200

    Remove old bidirectional relayers

--- crates/relayer-components-extra/src/relay/components/auto_relayers/mod.rs
+++ crates/relayer-components-extra/src/relay/components/auto_relayers/mod.rs
@@ -1,3 +1,2 @@
-pub mod parallel_bidirectional;
 pub mod parallel_event;
 pub mod parallel_two_way;
--- crates/relayer-components/src/relay/components/auto_relayers/mod.rs
+++ crates/relayer-components/src/relay/components/auto_relayers/mod.rs
@@ -1,5 +1,4 @@
 pub mod bidirectional;
-pub mod concurrent_bidirectional;
 pub mod concurrent_event;
 pub mod concurrent_two_way;
 pub mod sequential_event;
