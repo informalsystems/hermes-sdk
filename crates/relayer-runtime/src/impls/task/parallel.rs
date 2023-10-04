aa81902834b9da5261d93fbc63884d7486cdd17e
Soares Chen <soares.chen@maybevoid.com>
2023-10-04 08:35:05 +0200

    Improve handling of finished parallel tasks in JoinSet

--- crates/relayer-runtime/src/impls/task/parallel.rs
+++ crates/relayer-runtime/src/impls/task/parallel.rs
@@ -1,4 +1,7 @@
+use core::task::{Context, Poll};
+
 use futures::stream::{Stream, StreamExt};
+use futures::task::noop_waker;
 use ibc_relayer_components::runtime::traits::task::Task;
 use tokio::task::JoinSet;
 
@@ -23,15 +26,24 @@ where
 {
     let mut join_set = JoinSet::new();
 
+    let waker = noop_waker();
+
     tasks
-        .for_each(|task| {
+        .for_each_concurrent(None, |task| {
             join_set.spawn(async move {
                 task.run().await;
             });
+
+            let mut context = Context::from_waker(&waker);
+
+            // Remove finished tasks from the JoinSet to avoid accumulation of
+            // tasks from a potentially infinite stream.
+            while let Poll::Ready(Some(_)) = join_set.poll_join_next(&mut context) {}
 
             async {}
         })
         .await;
 
+    // Wait for all tasks to complete once the stream ends.
     while join_set.join_next().await.is_some() {}
 }
