20d76f497f1d992da63862ff45a752e95898472c
Soares Chen <soares.chen@maybevoid.com>
2023-10-10 17:54:32 +0200

    Refine trait bounds for extra chain components

--- crates/relayer-components-extra/src/components/extra/closures/chain.rs
+++ crates/relayer-components-extra/src/components/extra/closures/chain.rs
@@ -1,6 +1,18 @@
-use cgp_core::{HasComponents, HasErrorType};
+use cgp_core::HasComponents;
+use ibc_relayer_components::chain::traits::components::ack_packet_message_builder::{
+    AckPacketMessageBuilder, CanBuildAckPacketMessage,
+};
+use ibc_relayer_components::chain::traits::components::ack_packet_payload_builder::{
+    AckPacketPayloadBuilder, CanBuildAckPacketPayload,
+};
 use ibc_relayer_components::chain::traits::components::chain_status_querier::{
     CanQueryChainStatus, ChainStatusQuerier,
+};
+use ibc_relayer_components::chain::traits::components::client_state_querier::{
+    CanQueryClientState, ClientStateQuerier,
+};
+use ibc_relayer_components::chain::traits::components::consensus_state_height_querier::{
+    CanQueryConsensusStateHeight, ConsensusStateHeightQuerier,
 };
 use ibc_relayer_components::chain::traits::components::consensus_state_querier::{
     CanQueryConsensusState, ConsensusStateQuerier,
@@ -8,33 +20,133 @@ use ibc_relayer_components::chain::traits::components::consensus_state_querier::
 use ibc_relayer_components::chain::traits::components::message_sender::{
     CanSendMessages, MessageSender,
 };
+use ibc_relayer_components::chain::traits::components::packet_fields_reader::{
+    CanReadPacketFields, PacketFieldsReader,
+};
+use ibc_relayer_components::chain::traits::components::receive_packet_message_builder::{
+    CanBuildReceivePacketMessage, ReceivePacketMessageBuilder,
+};
+use ibc_relayer_components::chain::traits::components::receive_packet_payload_builder::{
+    CanBuildReceivePacketPayload, ReceivePacketPayloadBuilder,
+};
+use ibc_relayer_components::chain::traits::components::received_packet_querier::{
+    CanQueryReceivedPacket, ReceivedPacketQuerier,
+};
+use ibc_relayer_components::chain::traits::components::timeout_unordered_packet_message_builder::{
+    CanBuildTimeoutUnorderedPacketMessage, CanBuildTimeoutUnorderedPacketPayload,
+    TimeoutUnorderedPacketMessageBuilder, TimeoutUnorderedPacketPayloadBuilder,
+};
+use ibc_relayer_components::chain::traits::components::update_client_message_builder::{
+    CanBuildUpdateClientMessage, UpdateClientMessageBuilder,
+};
+use ibc_relayer_components::chain::traits::components::update_client_payload_builder::{
+    CanBuildUpdateClientPayload, UpdateClientPayloadBuilder,
+};
+use ibc_relayer_components::chain::traits::logs::packet::CanLogChainPacket;
+use ibc_relayer_components::chain::traits::types::chain_id::HasChainId;
+use ibc_relayer_components::chain::traits::types::client_state::{
+    HasClientStateFields, HasClientStateType,
+};
 use ibc_relayer_components::chain::traits::types::consensus_state::HasConsensusStateType;
-use ibc_relayer_components::chain::traits::types::height::HasHeightType;
-use ibc_relayer_components::chain::traits::types::ibc::HasIbcChainTypes;
+use ibc_relayer_components::chain::traits::types::height::{CanIncrementHeight, HasHeightType};
+use ibc_relayer_components::chain::traits::types::ibc::{
+    HasCounterpartyMessageHeight, HasIbcChainTypes,
+};
+use ibc_relayer_components::chain::traits::types::ibc_events::write_ack::HasWriteAckEvent;
+use ibc_relayer_components::chain::traits::types::packet::HasIbcPacketTypes;
+use ibc_relayer_components::chain::traits::types::packets::ack::HasAckPacketPayload;
+use ibc_relayer_components::chain::traits::types::packets::receive::HasReceivePacketPayload;
+use ibc_relayer_components::chain::traits::types::packets::timeout::HasTimeoutUnorderedPacketPayload;
 use ibc_relayer_components::chain::traits::types::status::HasChainStatusType;
+use ibc_relayer_components::chain::traits::types::update_client::HasUpdateClientPayload;
+use ibc_relayer_components::logger::traits::has_logger::HasLoggerType;
+use ibc_relayer_components::runtime::traits::runtime::HasRuntime;
 
 use crate::components::extra::chain::ExtraChainComponents;
 use crate::telemetry::traits::metrics::HasBasicMetrics;
 use crate::telemetry::traits::telemetry::HasTelemetry;
 
 pub trait UseExtraChainComponents<Counterparty>:
-    CanQueryChainStatus + CanQueryConsensusState<Counterparty> + CanSendMessages
+    HasRuntime
+    + HasChainId
+    + HasLoggerType
+    + CanIncrementHeight
+    + CanSendMessages
+    + CanQueryChainStatus
+    + HasConsensusStateType<Counterparty>
+    + HasClientStateFields<Counterparty>
+    + HasCounterpartyMessageHeight<Counterparty>
+    + CanLogChainPacket<Counterparty>
+    + CanQueryClientState<Counterparty>
+    + CanQueryConsensusState<Counterparty>
+    + CanQueryConsensusStateHeight<Counterparty>
+    + CanQueryReceivedPacket<Counterparty>
+    + CanReadPacketFields<Counterparty>
+    + CanBuildUpdateClientPayload<Counterparty>
+    + CanBuildUpdateClientMessage<Counterparty>
+    + CanBuildReceivePacketPayload<Counterparty>
+    + CanBuildReceivePacketMessage<Counterparty>
+    + CanBuildAckPacketPayload<Counterparty>
+    + CanBuildAckPacketMessage<Counterparty>
+    + CanBuildTimeoutUnorderedPacketPayload<Counterparty>
+    + CanBuildTimeoutUnorderedPacketMessage<Counterparty>
 where
-    Counterparty: HasHeightType + HasConsensusStateType<Self>,
+    Counterparty: HasHeightType
+        + HasClientStateType<Self>
+        + HasConsensusStateType<Self>
+        + HasIbcChainTypes<Self>
+        + HasUpdateClientPayload<Self>
+        + HasReceivePacketPayload<Self>
+        + HasAckPacketPayload<Self>
+        + HasTimeoutUnorderedPacketPayload<Self>,
 {
 }
 
-impl<Chain, Counterparty, BaseComponents> UseExtraChainComponents<Counterparty> for Chain
+impl<Chain, Counterparty, ChainComponents> UseExtraChainComponents<Counterparty> for Chain
 where
-    Counterparty: HasHeightType + HasConsensusStateType<Chain>,
-    Chain: HasErrorType
+    Chain: HasRuntime
+        + HasChainId
+        + HasLoggerType
+        + CanIncrementHeight
         + HasTelemetry
         + HasChainStatusType
+        + HasConsensusStateType<Counterparty>
+        + HasClientStateFields<Counterparty>
+        + HasCounterpartyMessageHeight<Counterparty>
+        + CanLogChainPacket<Counterparty>
         + HasIbcChainTypes<Counterparty>
-        + HasComponents<Components = ExtraChainComponents<BaseComponents>>,
+        + HasClientStateType<Counterparty>
+        + HasIbcChainTypes<Counterparty>
+        + HasIbcPacketTypes<Counterparty>
+        + HasUpdateClientPayload<Counterparty>
+        + HasReceivePacketPayload<Counterparty>
+        + HasWriteAckEvent<Counterparty>
+        + HasAckPacketPayload<Counterparty>
+        + HasTimeoutUnorderedPacketPayload<Counterparty>
+        + HasComponents<Components = ExtraChainComponents<ChainComponents>>,
+    Counterparty: HasHeightType
+        + HasIbcChainTypes<Chain>
+        + HasClientStateType<Chain>
+        + HasConsensusStateType<Chain>
+        + HasUpdateClientPayload<Chain>
+        + HasAckPacketPayload<Chain>
+        + HasTimeoutUnorderedPacketPayload<Chain>
+        + HasReceivePacketPayload<Chain>,
     Chain::Telemetry: HasBasicMetrics,
-    BaseComponents: MessageSender<Chain>
+    ChainComponents: MessageSender<Chain>
         + ChainStatusQuerier<Chain>
-        + ConsensusStateQuerier<Chain, Counterparty>,
+        + ConsensusStateQuerier<Chain, Counterparty>
+        + ClientStateQuerier<Chain, Counterparty>
+        + PacketFieldsReader<Chain, Counterparty>
+        + ConsensusStateHeightQuerier<Chain, Counterparty>
+        + ReceivedPacketQuerier<Chain, Counterparty>
+        + UpdateClientPayloadBuilder<Chain, Counterparty>
+        + UpdateClientMessageBuilder<Chain, Counterparty>
+        + ReceivePacketPayloadBuilder<Chain, Counterparty>
+        + ReceivePacketMessageBuilder<Chain, Counterparty>
+        + AckPacketPayloadBuilder<Chain, Counterparty>
+        + AckPacketMessageBuilder<Chain, Counterparty>
+        + TimeoutUnorderedPacketPayloadBuilder<Chain, Counterparty>
+        + TimeoutUnorderedPacketMessageBuilder<Chain, Counterparty>,
 {
 }
