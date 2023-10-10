1816a5d97b5fee48bc4432da29dc20d88b4f0621
Soares Chen <soares.chen@maybevoid.com>
2023-10-10 20:05:22 +0200

    Add dependency closure for ibc message sender

--- crates/relayer-components-extra/src/components/extra/closures/chain/mod.rs
+++ crates/relayer-components-extra/src/components/extra/closures/chain/mod.rs
@@ -1,3 +1,4 @@
 pub mod all;
 pub mod event_relayer;
+pub mod message_sender;
 pub mod packet_relayer;
---
+++ crates/relayer-components-extra/src/components/extra/closures/relay/channel_handshake.rs
@@ -0,0 +1,67 @@
+use cgp_core::HasComponents;
+use ibc_relayer_components::chain::traits::components::chain_status_querier::CanQueryChainHeight;
+use ibc_relayer_components::chain::traits::components::channel_handshake_message_builder::CanBuildChannelHandshakeMessages;
+use ibc_relayer_components::chain::traits::components::channel_handshake_payload_builder::CanBuildChannelHandshakePayloads;
+use ibc_relayer_components::chain::traits::components::client_state_querier::CanQueryClientState;
+use ibc_relayer_components::chain::traits::components::message_sender::CanSendMessages;
+use ibc_relayer_components::chain::traits::types::channel::HasInitChannelOptionsType;
+use ibc_relayer_components::chain::traits::types::ibc::HasIbcChainTypes;
+use ibc_relayer_components::chain::traits::types::ibc_events::channel::{
+    HasChannelOpenInitEvent, HasChannelOpenTryEvent,
+};
+use ibc_relayer_components::relay::impls::channel::open_ack::RelayChannelOpenAck;
+use ibc_relayer_components::relay::impls::channel::open_confirm::RelayChannelOpenConfirm;
+use ibc_relayer_components::relay::impls::channel::open_init::CanRaiseMissingChannelInitEventError;
+use ibc_relayer_components::relay::impls::channel::open_try::CanRaiseMissingChannelTryEventError;
+use ibc_relayer_components::relay::traits::chains::HasRelayChains;
+use ibc_relayer_components::relay::traits::channel::open_ack::ChannelOpenAckRelayer;
+use ibc_relayer_components::relay::traits::channel::open_confirm::ChannelOpenConfirmRelayer;
+use ibc_relayer_components::relay::traits::channel::open_handshake::CanRelayChannelOpenHandshake;
+use ibc_relayer_components::relay::traits::channel::open_init::{
+    CanInitChannel, ChannelInitializer,
+};
+use ibc_relayer_components::relay::traits::channel::open_try::ChannelOpenTryRelayer;
+use ibc_relayer_components::relay::traits::components::ibc_message_sender::{
+    CanSendSingleIbcMessage, IbcMessageSender, MainSink,
+};
+use ibc_relayer_components::relay::traits::target::{DestinationTarget, SourceTarget};
+
+use crate::components::extra::relay::ExtraRelayComponents;
+
+pub trait UseExtraChannelHandshakeRelayer: CanInitChannel + CanRelayChannelOpenHandshake
+where
+    Self::SrcChain: HasInitChannelOptionsType<Self::DstChain>,
+{
+}
+
+impl<Relay, SrcChain, DstChain, RelayComponents> UseExtraChannelHandshakeRelayer for Relay
+where
+    Relay: HasRelayChains<SrcChain = SrcChain, DstChain = DstChain>
+        + HasComponents<Components = ExtraRelayComponents<RelayComponents>>
+        + CanRaiseMissingChannelInitEventError
+        + CanRaiseMissingChannelTryEventError
+        + CanSendSingleIbcMessage<MainSink, DestinationTarget>,
+    RelayComponents: ChannelOpenTryRelayer<Relay>
+        + ChannelOpenAckRelayer<Relay>
+        + ChannelOpenConfirmRelayer<Relay>
+        + ChannelInitializer<Relay>
+        + IbcMessageSender<Relay, MainSink, SourceTarget>
+        + IbcMessageSender<Relay, MainSink, DestinationTarget>,
+    SrcChain: HasIbcChainTypes<DstChain>
+        + HasInitChannelOptionsType<DstChain>
+        + CanSendMessages
+        + CanBuildChannelHandshakePayloads<DstChain>
+        + CanBuildChannelHandshakeMessages<DstChain>
+        + HasChannelOpenInitEvent<DstChain>
+        + CanQueryChainHeight,
+    DstChain: HasIbcChainTypes<SrcChain>
+        + CanQueryClientState<SrcChain>
+        + HasChannelOpenTryEvent<SrcChain>
+        + CanBuildChannelHandshakePayloads<SrcChain>
+        + CanBuildChannelHandshakeMessages<SrcChain>,
+    SrcChain::ChannelId: Clone,
+    DstChain::ChannelId: Clone,
+    RelayChannelOpenAck: ChannelOpenAckRelayer<Relay>,
+    RelayChannelOpenConfirm: ChannelOpenConfirmRelayer<Relay>,
+{
+}
