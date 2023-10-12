0ace63098e08e3413b75cfce6d6aac67ad28f191
Soares Chen <soares.chen@maybevoid.com>
2023-10-12 20:01:46 +0200

    Remove ibc-relayer-all-in-one crate

--- crates/relayer-all-in-one/src/docs/mod.rs
+++
@@ -1,5 +0,0 @@
-/*!
-   Documentation-only module to contain long-form documentation and tutorials.
-*/
-
-pub mod context_generic_programming;
--- crates/relayer-all-in-one/src/one_for_all/impls/birelay/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod components;
-pub mod error;
-pub mod logger;
-pub mod runtime;
-pub mod two_way;
