ec3b7da55bc4d3f3997e43cc41608727e256bdbb
Soares Chen <soares.chen@maybevoid.com>
2023-12-08 13:40:11 +0100

    Implement Tokio ChildProcess type provider

--- crates/relayer-runtime/src/impls/runtime/components.rs
+++ crates/relayer-runtime/src/impls/runtime/components.rs
@@ -3,13 +3,15 @@ use ibc_test_components::runtime::traits::child_process::ChildProcessStarterComp
 use ibc_test_components::runtime::traits::exec_command::CommandExecutorComponent;
 use ibc_test_components::runtime::traits::read_file::FileAsStringReaderComponent;
 use ibc_test_components::runtime::traits::reserve_port::TcpPortReserverComponent;
+use ibc_test_components::runtime::traits::types::child_process::ChildProcessTypeComponent;
 use ibc_test_components::runtime::traits::types::file_path::FilePathTypeComponent;
 use ibc_test_components::runtime::traits::write_file::StringToFileWriterComponent;
 use tokio_runtime_components::impls::child_process::StartTokioChildProcess;
 use tokio_runtime_components::impls::exec_command::TokioExecCommand;
-use tokio_runtime_components::impls::file_path::ProvideStdPathType;
 use tokio_runtime_components::impls::read_file::TokioReadFileAsString;
 use tokio_runtime_components::impls::reserve_port::TokioReserveTcpPort;
+use tokio_runtime_components::impls::types::child_process::ProvideTokioChildProcessType;
+use tokio_runtime_components::impls::types::file_path::ProvideStdPathType;
 use tokio_runtime_components::impls::write_file::TokioWriteStringToFile;
 
 use crate::types::runtime::TokioRuntimeContext;
@@ -23,6 +25,7 @@ impl HasComponents for TokioRuntimeContext {
 delegate_components!(
     TokioRuntimeComponents;
     FilePathTypeComponent: ProvideStdPathType,
+    ChildProcessTypeComponent: ProvideTokioChildProcessType,
     ChildProcessStarterComponent: StartTokioChildProcess,
     FileAsStringReaderComponent: TokioReadFileAsString,
     CommandExecutorComponent: TokioExecCommand,
