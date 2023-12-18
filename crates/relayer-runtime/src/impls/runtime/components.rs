07e8f41738e7cc3905789e1c67b467bb12b8ce14
Soares Chen <soares.chen@maybevoid.com>
2023-12-19 00:15:48 +0100

    Group runtime components in tokio-runtime-components crate

--- crates/relayer-runtime/src/impls/runtime/components.rs
+++ crates/relayer-runtime/src/impls/runtime/components.rs
@@ -7,31 +7,27 @@ use ibc_test_components::runtime::traits::reserve_port::TcpPortReserverComponent
 use ibc_test_components::runtime::traits::types::child_process::ChildProcessTypeComponent;
 use ibc_test_components::runtime::traits::types::file_path::FilePathTypeComponent;
 use ibc_test_components::runtime::traits::write_file::StringToFileWriterComponent;
-use tokio_runtime_components::impls::child_process::StartTokioChildProcess;
-use tokio_runtime_components::impls::exec_command::TokioExecCommand;
-use tokio_runtime_components::impls::read_file::TokioReadFileAsString;
-use tokio_runtime_components::impls::reserve_port::TokioReserveTcpPort;
-use tokio_runtime_components::impls::sleep::TokioSleep;
-use tokio_runtime_components::impls::types::child_process::ProvideTokioChildProcessType;
-use tokio_runtime_components::impls::types::file_path::ProvideStdPathType;
-use tokio_runtime_components::impls::write_file::TokioWriteStringToFile;
+use tokio_runtime_components::components::runtime::TokioRuntimeComponents;
 
 use crate::types::runtime::TokioRuntimeContext;
 
-pub struct TokioRuntimeComponents;
+pub struct RelayerRuntimeComponents;
 
 impl HasComponents for TokioRuntimeContext {
-    type Components = TokioRuntimeComponents;
+    type Components = RelayerRuntimeComponents;
 }
 
 delegate_components!(
-    TokioRuntimeComponents;
-    FilePathTypeComponent: ProvideStdPathType,
-    ChildProcessTypeComponent: ProvideTokioChildProcessType,
-    ChildProcessStarterComponent: StartTokioChildProcess,
-    FileAsStringReaderComponent: TokioReadFileAsString,
-    CommandExecutorComponent: TokioExecCommand,
-    StringToFileWriterComponent: TokioWriteStringToFile,
-    TcpPortReserverComponent: TokioReserveTcpPort,
-    SleeperComponent: TokioSleep,
+    RelayerRuntimeComponents;
+    [
+        SleeperComponent,
+        FilePathTypeComponent,
+        ChildProcessTypeComponent,
+        ChildProcessStarterComponent,
+        FileAsStringReaderComponent,
+        CommandExecutorComponent,
+        StringToFileWriterComponent,
+        TcpPortReserverComponent,
+    ]:
+        TokioRuntimeComponents,
 );
---
+++ crates/tokio-runtime-components/src/components/runtime.rs
@@ -0,0 +1,32 @@
+use cgp_core::prelude::*;
+use ibc_relayer_components::runtime::traits::sleep::SleeperComponent;
+use ibc_test_components::runtime::traits::child_process::ChildProcessStarterComponent;
+use ibc_test_components::runtime::traits::exec_command::CommandExecutorComponent;
+use ibc_test_components::runtime::traits::read_file::FileAsStringReaderComponent;
+use ibc_test_components::runtime::traits::reserve_port::TcpPortReserverComponent;
+use ibc_test_components::runtime::traits::types::child_process::ChildProcessTypeComponent;
+use ibc_test_components::runtime::traits::types::file_path::FilePathTypeComponent;
+use ibc_test_components::runtime::traits::write_file::StringToFileWriterComponent;
+
+use crate::impls::child_process::StartTokioChildProcess;
+use crate::impls::exec_command::TokioExecCommand;
+use crate::impls::read_file::TokioReadFileAsString;
+use crate::impls::reserve_port::TokioReserveTcpPort;
+use crate::impls::sleep::TokioSleep;
+use crate::impls::types::child_process::ProvideTokioChildProcessType;
+use crate::impls::types::file_path::ProvideStdPathType;
+use crate::impls::write_file::TokioWriteStringToFile;
+
+pub struct TokioRuntimeComponents;
+
+delegate_components!(
+    TokioRuntimeComponents;
+    FilePathTypeComponent: ProvideStdPathType,
+    ChildProcessTypeComponent: ProvideTokioChildProcessType,
+    ChildProcessStarterComponent: StartTokioChildProcess,
+    FileAsStringReaderComponent: TokioReadFileAsString,
+    CommandExecutorComponent: TokioExecCommand,
+    StringToFileWriterComponent: TokioWriteStringToFile,
+    TcpPortReserverComponent: TokioReserveTcpPort,
+    SleeperComponent: TokioSleep,
+);
