2ca028ca7c2c8fd3bcc9837fa326c5d9c881488d
Soares Chen <soares.chen@maybevoid.com>
2024-01-22 10:38:57 +0100

    Implement InitCelestiaBridge with exec_command_with_envs

--- crates/celestia/celestia-integration-tests/src/bootstrap/impls/initializers/init_bridge.rs
+++ crates/celestia/celestia-integration-tests/src/bootstrap/impls/initializers/init_bridge.rs
@@ -1,10 +1,9 @@
 use cgp_core::prelude::*;
-use hermes_cosmos_test_components::bootstrap::traits::fields::chain_command_path::HasChainCommandPath;
 use hermes_cosmos_test_components::bootstrap::traits::initializers::init_chain_data::ChainDataInitializer;
 use hermes_relayer_components::chain::traits::types::chain_id::HasChainIdType;
 use hermes_relayer_components::runtime::traits::runtime::HasRuntime;
 use hermes_test_components::chain_driver::traits::types::chain::HasChainType;
-use hermes_test_components::runtime::traits::exec_command::CanExecCommand;
+use hermes_test_components::runtime::traits::exec_command::CanExecCommandWithEnvs;
 use hermes_test_components::runtime::traits::types::file_path::HasFilePathType;
 
 pub struct InitCelestiaBridge;
@@ -13,7 +12,7 @@ pub struct InitCelestiaBridge;
 impl<Bootstrap, Runtime, Chain> ChainDataInitializer<Bootstrap> for InitCelestiaBridge
 where
     Bootstrap: HasChainType<Chain = Chain> + HasRuntime<Runtime = Runtime>,
-    Runtime: HasFilePathType + CanExecCommand,
+    Runtime: HasFilePathType + CanExecCommandWithEnvs,
     Chain: HasChainIdType,
 {
     async fn init_chain_data(
@@ -23,11 +22,15 @@ where
     ) -> Result<(), Bootstrap::Error> {
         let chain_id = chain_id.to_string();
 
+        let bridge_home_dir =
+            Runtime::join_file_path(chain_home_dir, &Runtime::file_path_from_string("bridge"));
+
         bootstrap
             .runtime()
-            .exec_command(
+            .exec_command_with_envs(
                 &Runtime::file_path_from_string("celestia"),
                 &["bridge", "init", "--p2p.network", &chain_id],
+                &[("HOME", &Runtime::file_path_to_string(&bridge_home_dir))],
             )
             .await
             .map_err(Bootstrap::raise_error)?;
