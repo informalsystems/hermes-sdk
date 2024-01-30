cb92b38bf358de702ca44ce7ed70017a72830b0a
Soares Chen <soares.chen@maybevoid.com>
2024-01-30 21:11:52 +0100

    Break down HasTxTypes into individual traits

--- crates/cosmos/cosmos-relayer/src/impls/transaction/types.rs
+++ crates/cosmos/cosmos-relayer/src/impls/transaction/types.rs
@@ -1,8 +1,10 @@
+use cgp_core::Async;
 use futures::lock::MutexGuard;
 use hermes_relayer_components::runtime::traits::runtime::ProvideRuntime;
 use hermes_relayer_components::transaction::traits::nonce::guard::HasNonceGuard;
 use hermes_relayer_components::transaction::traits::types::{
-    HasNonceType, HasSignerType, HasTxTypes,
+    ProvideFeeType, ProvideNonceType, ProvideSignerType, ProvideTransactionHashType,
+    ProvideTransactionType, ProvideTxResponseType,
 };
 use hermes_relayer_runtime::types::runtime::HermesRuntime;
 use ibc_proto::cosmos::tx::v1beta1::{Fee, TxRaw};
@@ -22,30 +24,25 @@ impl ProvideRuntime<CosmosTxContext> for CosmosTxComponents {
     }
 }
 
-impl HasSignerType for CosmosTxContext {
+impl<Chain> ProvideSignerType<Chain> for CosmosTxComponents
+where
+    Chain: Async,
+{
     type Signer = Secp256k1KeyPair;
 }
 
-impl HasNonceType for CosmosTxContext {
+impl<Chain> ProvideNonceType<Chain> for CosmosTxComponents
+where
+    Chain: Async,
+{
     type Nonce = Account;
 }
 
-impl HasNonceGuard for CosmosTxContext {
-    type NonceGuard<'a> = (MutexGuard<'a, ()>, Account);
-
-    fn deref_nonce<'a, 'b>((_, nonce): &'a Self::NonceGuard<'b>) -> &'a Account {
-        nonce
-    }
-}
-
-impl HasTxTypes for CosmosTxContext {
+impl<Chain> ProvideTransactionType<Chain> for CosmosTxComponents
+where
+    Chain: Async,
+{
     type Transaction = SignedTx;
-
-    type Fee = Fee;
-
-    type TxHash = TxHash;
-
-    type TxResponse = TxResponse;
 
     fn tx_size(signed_tx: &SignedTx) -> usize {
         let tx_raw = TxRaw {
@@ -55,5 +52,34 @@ impl HasTxTypes for CosmosTxContext {
         };
 
         tx_raw.encoded_len()
+    }
+}
+
+impl<Chain> ProvideFeeType<Chain> for CosmosTxComponents
+where
+    Chain: Async,
+{
+    type Fee = Fee;
+}
+
+impl<Chain> ProvideTransactionHashType<Chain> for CosmosTxComponents
+where
+    Chain: Async,
+{
+    type TxHash = TxHash;
+}
+
+impl<Chain> ProvideTxResponseType<Chain> for CosmosTxComponents
+where
+    Chain: Async,
+{
+    type TxResponse = TxResponse;
+}
+
+impl HasNonceGuard for CosmosTxContext {
+    type NonceGuard<'a> = (MutexGuard<'a, ()>, Account);
+
+    fn deref_nonce<'a, 'b>((_, nonce): &'a Self::NonceGuard<'b>) -> &'a Account {
+        nonce
     }
 }
--- crates/relayer/relayer-components/src/transaction/traits/types.rs
+++ crates/relayer/relayer-components/src/transaction/traits/types.rs
@@ -1,28 +1,64 @@
-use cgp_core::{Async, HasErrorType};
+use cgp_core::prelude::*;
 
 use crate::chain::traits::types::event::HasEventType;
 use crate::chain::traits::types::message::HasMessageType;
 
+#[derive_component(NonceTypeComponent, ProvideNonceType<Chain>)]
 pub trait HasNonceType: Async {
     type Nonce: Async;
 }
 
+#[derive_component(SignerTypeComponent, ProvideSignerType<Chain>)]
 pub trait HasSignerType: Async {
     type Signer: Async;
 }
 
 pub type SignerOf<Context> = <Context as HasSignerType>::Signer;
 
-pub trait HasTxTypes:
-    HasMessageType + HasEventType + HasNonceType + HasSignerType + HasErrorType
-{
+#[derive_component(TransactionTypeComponent, ProvideTransactionType<Chain>)]
+pub trait HasTransactionType: Async {
     type Transaction: Async;
 
-    type Fee: Async;
-
-    type TxHash: Async;
-
-    type TxResponse: Async;
-
     fn tx_size(tx: &Self::Transaction) -> usize;
+}
+
+#[derive_component(FeeTypeComponent, ProvideFeeType<Chain>)]
+pub trait HasFeeType: Async {
+    type Fee: Async;
+}
+
+#[derive_component(TransactionHashTypeComponent, ProvideTransactionHashType<Chain>)]
+pub trait HasTransactionHashType: Async {
+    type TxHash: Async;
+}
+
+#[derive_component(TxResponseTypeComponent, ProvideTxResponseType<Chain>)]
+pub trait HasTxResponseType: Async {
+    type TxResponse: Async;
+}
+
+pub trait HasTxTypes:
+    HasMessageType
+    + HasEventType
+    + HasTransactionType
+    + HasNonceType
+    + HasFeeType
+    + HasSignerType
+    + HasTransactionHashType
+    + HasTxResponseType
+    + HasErrorType
+{
+}
+
+impl<Chain> HasTxTypes for Chain where
+    Chain: HasMessageType
+        + HasEventType
+        + HasTransactionType
+        + HasNonceType
+        + HasFeeType
+        + HasSignerType
+        + HasTransactionHashType
+        + HasTxResponseType
+        + HasErrorType
+{
 }
