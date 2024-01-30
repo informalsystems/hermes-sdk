4d8e41d4c68a92129b32a84d224c50fd3762f762
Soares Chen <soares.chen@maybevoid.com>
2024-01-30 21:44:54 +0100

    Add Sovereign fee type

--- crates/sovereign/sovereign-client-components/src/sovereign/impls/types/transaction.rs
+++ crates/sovereign/sovereign-client-components/src/sovereign/impls/types/transaction.rs
@@ -1,6 +1,8 @@
 use cgp_core::Async;
 use ed25519_dalek::SigningKey;
-use hermes_relayer_components::transaction::traits::types::{ProvideNonceType, ProvideSignerType};
+use hermes_relayer_components::transaction::traits::types::{
+    ProvideFeeType, ProvideNonceType, ProvideSignerType,
+};
 
 pub struct ProvideSovereignTransactionTypes;
 
@@ -9,6 +11,13 @@ where
     Chain: Async,
 {
     type Nonce = u64;
+}
+
+impl<Chain> ProvideFeeType<Chain> for ProvideSovereignTransactionTypes
+where
+    Chain: Async,
+{
+    type Fee = u64;
 }
 
 impl<Chain> ProvideSignerType<Chain> for ProvideSovereignTransactionTypes
