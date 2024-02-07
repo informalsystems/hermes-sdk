8870776eae8c8a949f89cce9a3e2f594d09ff63f
Soares Chen <soares.chen@maybevoid.com>
2024-02-07 16:42:27 +0100

    Sleep for 2 seconds before building the chain context

--- crates/cosmos/cosmos-integration-tests/src/impls/bootstrap/build_cosmos_chain.rs
+++ crates/cosmos/cosmos-integration-tests/src/impls/bootstrap/build_cosmos_chain.rs
@@ -1,8 +1,12 @@
+use core::time::Duration;
+
 use cgp_core::CanRaiseError;
 use hermes_cosmos_relayer::contexts::chain::CosmosChain;
 use hermes_cosmos_relayer::types::error::Error as CosmosError;
 use hermes_cosmos_test_components::bootstrap::traits::types::chain_node_config::HasChainNodeConfigType;
 use hermes_cosmos_test_components::chain_driver::types::wallet::CosmosTestWallet;
+use hermes_relayer_components::runtime::traits::runtime::HasRuntime;
+use hermes_relayer_components::runtime::traits::sleep::CanSleep;
 use hermes_test_components::chain_driver::traits::types::chain::HasChainType;
 use hermes_test_components::chain_driver::traits::types::wallet::HasWalletType;
 use hermes_test_components::driver::traits::types::chain_driver::HasChainDriverType;
@@ -20,8 +24,10 @@ where
         + HasChainNodeConfigType
         + CanBuildRelayerChainConfig
         + HasCosmosBuilder
+        + HasRuntime
         + CanRaiseError<CosmosError>,
     ChainDriver: HasWalletType<Wallet = CosmosTestWallet>,
+    Bootstrap::Runtime: CanSleep,
 {
     async fn build_chain_with_node_config(
         bootstrap: &Bootstrap,
@@ -30,6 +36,12 @@ where
     ) -> Result<CosmosChain, Bootstrap::Error> {
         let relayer_chain_config =
             bootstrap.build_relayer_chain_config(&chain_node_config, &relayer_wallet)?;
+
+        // TODO: Have a more reliable way to wait for the bootstrapped full node to
+        // start up. If we don't wait, the building of the chain would fail during
+        // the spawning of `ChainHandle`, as the v1 relayer tries to perform health
+        // check on building.
+        bootstrap.runtime().sleep(Duration::from_secs(2)).await;
 
         let chain = bootstrap
             .cosmos_builder()
