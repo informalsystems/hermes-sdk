d2360c407f449f2551736bf1455c235a0dd80cc6
Soares Chen <soares.chen@maybevoid.com>
2024-02-08 21:18:12 +0100

    Move Cosmos query components to queries:: module

--- crates/cosmos/cosmos-client-components/src/components/ibc_client.rs
+++ crates/cosmos/cosmos-client-components/src/components/ibc_client.rs
@@ -6,8 +6,8 @@ use hermes_relayer_components::chain::traits::queries::consensus_state::Consensu
 
 use crate::impls::client::create_client_message::BuildCosmosCreateClientMessage;
 use crate::impls::connection::connection_handshake_message::BuildCosmosConnectionHandshakeMessage;
-use crate::impls::query_client_state::QueryCosmosClientStateFromChainHandle;
-use crate::impls::query_consensus_state::QueryCosmosConsensusStateFromChainHandle;
+use crate::impls::queries::client_state::QueryCosmosClientStateFromChainHandle;
+use crate::impls::queries::consensus_state::QueryCosmosConsensusStateFromChainHandle;
 
 pub struct CosmosIbcClientComponents;
 
