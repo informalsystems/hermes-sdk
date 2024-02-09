f5bbd699e63286fc8bb71a526ee8af0368e07271
Luca Joss <43531661+ljoss17@users.noreply.github.com>
2024-02-09 13:52:59 +0100

    Add CLI to query all connections of a chain (#130)
    * Add CLI to query all connections of a chain
    
    * Fix comment
    
    * Improve query connections CLI

---
+++ crates/cli/cli/src/commands/query/mod.rs
@@ -0,0 +1,21 @@
+mod connections;
+pub use connections::QueryConnections;
+
+use hermes_cli_framework::command::Runnable;
+use hermes_cosmos_relayer::contexts::builder::CosmosBuilder;
+
+use crate::Result;
+
+#[derive(Debug, clap::Subcommand)]
+pub enum QueryCommands {
+    /// Query all connections
+    Connections(QueryConnections),
+}
+
+impl QueryCommands {
+    pub async fn run(&self, builder: CosmosBuilder) -> Result<()> {
+        match self {
+            Self::Connections(cmd) => cmd.run(builder).await,
+        }
+    }
+}
