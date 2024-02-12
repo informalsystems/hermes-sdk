46714031f2149f03a68c6416a65e0a411283d1a9
Sean Chen <seanchen11235@gmail.com>
2024-02-12 15:54:35 -0600

    Implement `query channels` command (#154)
    * Implement query channels command
    
    * Fix `self` leaking error
    
    * Remove unused `use` statement
    
    * Split up calls to `with_blocking_chain_handle` to facilite future generic implementation
    
    ---------
    
    Co-authored-by: Romain Ruetschi <106849+romac@users.noreply.github.com>

--- crates/cli/cli/src/commands/query/mod.rs
+++ crates/cli/cli/src/commands/query/mod.rs
@@ -7,12 +7,16 @@ pub use connection::QueryConnection;
 mod connections;
 pub use connections::QueryConnections;
 
+mod channels;
+pub use channels::QueryChannels;
+
 use hermes_cli_framework::command::Runnable;
 use hermes_cli_framework::output::Output;
 use hermes_cosmos_relayer::contexts::builder::CosmosBuilder;
 
 use crate::Result;
 
+/// All subcommands for querying IBC-related objects and data.
 #[derive(Debug, clap::Subcommand)]
 pub enum QueryCommands {
     /// Query information about IBC clients
@@ -21,6 +25,9 @@ pub enum QueryCommands {
 
     /// Query all connections
     Connections(QueryConnections),
+
+    /// Query all channels
+    Channels(QueryChannels),
 
     /// Query connection information
     #[clap(subcommand)]
@@ -32,6 +39,7 @@ impl Runnable for QueryCommands {
         match self {
             Self::Client(cmd) => cmd.run(builder).await,
             Self::Connections(cmd) => cmd.run(builder).await,
+            Self::Channels(cmd) => cmd.run(builder).await,
             Self::Connection(cmd) => cmd.run(builder).await,
         }
     }
