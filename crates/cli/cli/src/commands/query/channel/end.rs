74eaaebcd3897bcd72ba7c0359d8af486e52c151
Sean Chen <seanchen11235@gmail.com>
2024-02-15 03:34:51 -0600

    Implement `query channel` subcommands (#165)
    * Add query channel subcommand to QueryCommands
    
    * Add query/channal/mod.rs
    
    * Implement query channel end command
    
    * Save progress on QueryChannelEnds command
    
    * Implement QueryChannelClient command
    
    * Remove unnecessary function abstraction
    
    * Switch from Runnable to CommandRunner
    
    * Add & to CosmosBuilder params
    
    * Restructure query channel ends code a bit
    
    * Add missing space
    
    * Return explicit errors from queries instead of using `map_err`
    
    * Fix spacing of a bracket
    
    * Remove trailing whitespace

---
+++ crates/cli/cli/src/commands/query/channel/end.rs
@@ -0,0 +1,103 @@
+use oneline_eyre::eyre::eyre;
+
+use hermes_cli_framework::command::CommandRunner;
+use hermes_cli_framework::output::Output;
+
+use hermes_cosmos_client_components::traits::chain_handle::HasBlockingChainHandle;
+use hermes_cosmos_relayer::contexts::builder::CosmosBuilder;
+use hermes_cosmos_relayer::types::error::BaseError;
+
+use ibc_relayer::chain::handle::ChainHandle;
+use ibc_relayer::chain::requests::{IncludeProof, QueryChannelRequest, QueryHeight};
+use ibc_relayer_types::core::ics04_channel::channel::State;
+use ibc_relayer_types::core::ics24_host::identifier::{ChainId, ChannelId, PortId};
+use ibc_relayer_types::Height;
+
+use crate::Result;
+
+#[derive(Debug, clap::Parser)]
+pub struct QueryChannelEnd {
+    #[clap(
+        long = "chain",
+        required = true,
+        value_name = "CHAIN_ID",
+        help_heading = "REQUIRED",
+        help = "Identifier of the chain to query"
+    )]
+    chain_id: ChainId,
+
+    #[clap(
+        long = "port",
+        required = true,
+        value_name = "PORT_ID",
+        help_heading = "REQUIRED",
+        help = "Identifier of the port to query"
+    )]
+    port_id: PortId,
+
+    #[clap(
+        long = "channel",
+        required = true,
+        value_name = "CHANNEL_ID",
+        help_heading = "REQUIRED",
+        help = "Identifier of the channel to query"
+    )]
+    channel_id: ChannelId,
+
+    #[clap(
+        long = "height",
+        value_name = "HEIGHT",
+        help = "Height of the state to query. Leave unspecified for the latest height."
+    )]
+    height: Option<u64>,
+}
+
+impl CommandRunner<CosmosBuilder> for QueryChannelEnd {
+    async fn run(&self, builder: &CosmosBuilder) -> Result<Output> {
+        let chain = builder.build_chain(&self.chain_id).await?;
+        let channel_id = self.channel_id.clone();
+        let port_id = self.port_id.clone();
+        let height = self.height;
+
+        let query_height = if let Some(height) = height {
+            let specified_height = Height::new(chain.chain_id.version(), height)
+                .map_err(|e| BaseError::generic(eyre!("Failed to create Height with revision number `{}` and revision height `{height}`. Error: {e}", chain.chain_id.version())))?;
+
+            QueryHeight::Specific(specified_height)
+        } else {
+            QueryHeight::Latest
+        };
+
+        let channel_end = chain
+            .with_blocking_chain_handle(move |chain_handle| {
+                match chain_handle.query_channel(
+                    QueryChannelRequest {
+                        port_id,
+                        channel_id,
+                        height: query_height,
+                    },
+                    IncludeProof::No,
+                ) {
+                    Ok((channel_end, _)) => Ok(channel_end),
+                    Err(e) => Err(BaseError::relayer(e).into()),
+                }
+            })
+            .await;
+
+        match channel_end {
+            Ok(channel_end) => {
+                if channel_end.state_matches(&State::Uninitialized) {
+                    Err(BaseError::generic(eyre!(
+                        "port '{}' & channel '{}' do not exist",
+                        self.port_id,
+                        self.channel_id,
+                    ))
+                    .into())
+                } else {
+                    Ok(Output::success(channel_end))
+                }
+            }
+            Err(e) => Err(e.into()),
+        }
+    }
+}
---
+++ crates/cli/cli/src/commands/query/channel/mod.rs
@@ -0,0 +1,30 @@
+mod client;
+mod end;
+mod ends;
+
+pub use client::QueryChannelClient;
+pub use end::QueryChannelEnd;
+pub use ends::QueryChannelEnds;
+
+use hermes_cli_framework::command::CommandRunner;
+use hermes_cli_framework::output::Output;
+use hermes_cosmos_relayer::contexts::builder::CosmosBuilder;
+
+use crate::Result;
+
+#[derive(Debug, clap::Subcommand)]
+pub enum QueryChannel {
+    End(QueryChannelEnd),
+    Ends(QueryChannelEnds),
+    Client(QueryChannelClient),
+}
+
+impl QueryChannel {
+    pub async fn run(&self, builder: &CosmosBuilder) -> Result<Output> {
+        match self {
+            Self::End(cmd) => cmd.run(builder).await,
+            Self::Ends(cmd) => cmd.run(builder).await,
+            Self::Client(cmd) => cmd.run(builder).await,
+        }
+    }
+}
