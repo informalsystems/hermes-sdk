c2df2f04f7f33b36a64f3a957436bfa56f5d6bc7
Sean Chen <seanchen11235@gmail.com>
2024-02-21 11:00:05 -0600

    Clean up query packet pending acks command

--- crates/cli/cli/src/commands/query/packet/pending_acks.rs
+++ crates/cli/cli/src/commands/query/packet/pending_acks.rs
@@ -1,14 +1,17 @@
+use cgp_core::CanRaiseError;
+use hermes_cosmos_relayer::contexts::chain::CosmosChain;
+use oneline_eyre::eyre::{eyre, Context};
+
 use hermes_cli_framework::command::CommandRunner;
 use hermes_cli_framework::output::{json, Output};
 use hermes_cosmos_client_components::traits::chain_handle::HasBlockingChainHandle;
 use hermes_cosmos_relayer::contexts::builder::CosmosBuilder;
-use hermes_cosmos_relayer::types::error::BaseError;
+
 use ibc_relayer::chain::counterparty::{channel_connection_client, unreceived_acknowledgements};
 use ibc_relayer::path::PathIdentifiers;
 use ibc_relayer_types::core::ics04_channel::packet::Sequence;
 use ibc_relayer_types::core::ics24_host::identifier::{ChainId, ChannelId, PortId};
 use ibc_relayer_types::Height;
-use oneline_eyre::eyre::{eyre, Context};
 
 use crate::Result;
 
@@ -48,29 +51,34 @@ impl QueryPendingAcks {
     async fn execute(&self, builder: &CosmosBuilder) -> Result<Option<(Vec<Sequence>, Height)>> {
         let port_id = self.port_id.clone();
         let channel_id = self.channel_id.clone();
-        let chain = builder.build_chain(&self.chain_id).await?;
+        let chain = builder
+            .build_chain(&self.chain_id)
+            .await
+            .wrap_err_with(|| format!("failed to build chain `{}`", self.chain_id))?;
 
         let chan_conn_cli = chain
             .with_blocking_chain_handle(move |handle| {
                 let chan_conn_cli = channel_connection_client(&handle, &port_id, &channel_id)
                     .map_err(|e| {
-                        BaseError::generic(eyre!(
-                            "failed to get channel connection and client: {}",
-                            e
-                        ))
+                        CosmosChain::raise_error(
+                            e.1.wrap_err("failed to get channel connection and client"),
+                        )
                     })?;
                 Ok(chan_conn_cli)
             })
             .await?;
 
         let counterparty_chain_id = chan_conn_cli.client.client_state.chain_id();
-        let counterparty_chain = builder.build_chain(&counterparty_chain_id.clone()).await?;
+        let counterparty_chain = builder
+            .build_chain(&counterparty_chain_id.clone())
+            .await
+            .wrap_err_with(|| format!("failed to build chain `{}`", counterparty_chain_id))?;
 
         let channel = chan_conn_cli.channel.clone();
         let path_identifiers =
             PathIdentifiers::from_channel_end(channel.clone()).ok_or_else(|| {
-                BaseError::generic(eyre!(
-                    "failed to get the path identifiers for channel ({}, {})",
+                CosmosChain::raise_error(eyre!(
+                    "failed to get the path identifiers for channel (`{}`, `{}`)",
                     channel.channel_id,
                     channel.port_id
                 ))
