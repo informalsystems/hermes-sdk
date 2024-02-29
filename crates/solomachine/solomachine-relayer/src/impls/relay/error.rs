3192c4fcad1a5f398f593fbc359caa6c07580979
Soares Chen <soares.chen@maybevoid.com>
2024-02-29 21:32:27 +0100

    Use CosmosError in solomachine

--- crates/solomachine/solomachine-relayer/src/impls/relay/error.rs
+++ crates/solomachine/solomachine-relayer/src/impls/relay/error.rs
@@ -1,15 +1,15 @@
 use eyre::eyre;
+use hermes_cosmos_relayer::types::error::Error;
 use hermes_relayer_components::relay::impls::connection::open_init::CanRaiseMissingConnectionInitEventError;
 
 use crate::context::relay::SolomachineRelay;
 use crate::traits::solomachine::Solomachine;
-use crate::types::error::{BaseError, Error};
 
 impl<Chain> CanRaiseMissingConnectionInitEventError for SolomachineRelay<Chain>
 where
     Chain: Solomachine<Error = Error>,
 {
     fn missing_connection_init_event_error(&self) -> Self::Error {
-        BaseError::generic(eyre!("missing ConnectionOpenInit event")).into()
+        eyre!("missing ConnectionOpenInit event").into()
     }
 }
