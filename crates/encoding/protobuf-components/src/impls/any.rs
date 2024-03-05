12228b4d3d1c5bc236fb22ffa3367285c77e0a69
Soares Chen <soares.chen@maybevoid.com>
2024-03-05 21:44:56 +0100

    Move protobuf encoding components to hermes-protobuf-components crate

--- crates/encoding/protobuf-components/src/impls/any.rs
+++ crates/encoding/protobuf-components/src/impls/any.rs
@@ -1,3 +1,4 @@
+use core::fmt::Display;
 use core::marker::PhantomData;
 
 use cgp_core::{CanRaiseError, HasErrorType};
@@ -5,8 +6,8 @@ use hermes_relayer_components::encode::traits::decoder::Decoder;
 use hermes_relayer_components::encode::traits::encoded::HasEncodedType;
 use hermes_relayer_components::encode::traits::encoder::Encoder;
 use hermes_relayer_components::encode::traits::schema::HasSchema;
-use ibc_proto::google::protobuf::Any;
 use prost::{DecodeError, Message};
+use prost_types::Any;
 
 #[derive(Debug)]
 pub struct TypeUrlMismatchError {
@@ -18,17 +19,17 @@ pub struct EncodeAsAnyProtobuf<InEncoder>(pub PhantomData<InEncoder>);
 
 impl<InEncoder, Encoding, Value> Encoder<Encoding, Value> for EncodeAsAnyProtobuf<InEncoder>
 where
-    Encoding:
-        HasEncodedType<Encoded = Vec<u8>> + HasSchema<Value, Schema = &'static str> + HasErrorType,
+    Encoding: HasEncodedType<Encoded = Vec<u8>> + HasSchema<Value> + HasErrorType,
     InEncoder: Encoder<Encoding, Value>,
+    Encoding::Schema: Display,
 {
     fn encode(encoding: &Encoding, value: &Value) -> Result<Vec<u8>, Encoding::Error> {
         let encoded = InEncoder::encode(encoding, value)?;
-        let type_url = *encoding.schema(PhantomData::<Value>);
+        let type_url = encoding.schema(PhantomData::<Value>);
 
         let any = Any {
             value: encoded,
-            type_url: type_url.to_owned(),
+            type_url: type_url.to_string(),
         };
 
         Ok(any.encode_to_vec())
@@ -38,17 +39,18 @@ where
 impl<InEncoder, Encoding, Value> Decoder<Encoding, Value> for EncodeAsAnyProtobuf<InEncoder>
 where
     Encoding: HasEncodedType<Encoded = Vec<u8>>
-        + HasSchema<Value, Schema = &'static str>
+        + HasSchema<Value>
         + CanRaiseError<DecodeError>
         + CanRaiseError<TypeUrlMismatchError>,
     InEncoder: Decoder<Encoding, Value>,
+    Encoding::Schema: Display,
 {
     fn decode(encoding: &Encoding, encoded: &Vec<u8>) -> Result<Value, Encoding::Error> {
-        let type_url = *encoding.schema(PhantomData::<Value>);
+        let type_url = encoding.schema(PhantomData::<Value>).to_string();
 
         let any: Any = Message::decode(encoded.as_ref()).map_err(Encoding::raise_error)?;
 
-        if &any.type_url != type_url {
+        if &any.type_url != &type_url {
             return Err(Encoding::raise_error(TypeUrlMismatchError {
                 expected_url: type_url.to_owned(),
                 actual_url: any.type_url,
--- crates/encoding/protobuf-components/src/impls/wrap_any.rs
+++ crates/encoding/protobuf-components/src/impls/wrap_any.rs
@@ -3,30 +3,30 @@ use hermes_relayer_components::encode::traits::decoder::Decoder;
 use hermes_relayer_components::encode::traits::encoded::HasEncodedType;
 use hermes_relayer_components::encode::traits::encoder::Encoder;
 
-use crate::impls::encode::any::EncodeAsAnyProtobuf;
-use crate::impls::encode::from_context::EncodeFromContext;
+use crate::impls::any::EncodeAsAnyProtobuf;
+use crate::impls::from_context::EncodeFromContext;
 
 #[derive(Default, Clone)]
-pub struct WrapAny<Value> {
+pub struct Any<Value> {
     pub value: Value,
 }
 
 pub struct EncodeWrapAny;
 
-impl<Encoding, Value> Encoder<Encoding, WrapAny<Value>> for EncodeWrapAny
+impl<Encoding, Value> Encoder<Encoding, Any<Value>> for EncodeWrapAny
 where
     Encoding: HasEncodedType + HasErrorType,
     EncodeAsAnyProtobuf<EncodeFromContext>: Encoder<Encoding, Value>,
 {
     fn encode(
         encoding: &Encoding,
-        value: &WrapAny<Value>,
+        value: &Any<Value>,
     ) -> Result<Encoding::Encoded, Encoding::Error> {
         <EncodeAsAnyProtobuf<EncodeFromContext>>::encode(encoding, &value.value)
     }
 }
 
-impl<Encoding, Value> Decoder<Encoding, WrapAny<Value>> for EncodeWrapAny
+impl<Encoding, Value> Decoder<Encoding, Any<Value>> for EncodeWrapAny
 where
     Encoding: HasEncodedType + HasErrorType,
     EncodeAsAnyProtobuf<EncodeFromContext>: Decoder<Encoding, Value>,
@@ -34,9 +34,9 @@ where
     fn decode(
         encoding: &Encoding,
         encoded: &Encoding::Encoded,
-    ) -> Result<WrapAny<Value>, Encoding::Error> {
+    ) -> Result<Any<Value>, Encoding::Error> {
         let value = <EncodeAsAnyProtobuf<EncodeFromContext>>::decode(encoding, encoded)?;
 
-        Ok(WrapAny { value })
+        Ok(Any { value })
     }
 }
