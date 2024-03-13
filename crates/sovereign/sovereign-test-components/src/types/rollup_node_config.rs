71a65a1f162bd434d1ecbb93485dcd0b7c91a263
Soares Chen <soares.chen@maybevoid.com>
2024-03-13 14:01:28 +0100

    Revert "Rename start_height to genesis_height in rollup node config"
    This reverts commit e1cf2c2c8a8cc06e5239d363d6d9ae8e185a5c51.

--- crates/sovereign/sovereign-test-components/src/types/rollup_node_config.rs
+++ crates/sovereign/sovereign-test-components/src/types/rollup_node_config.rs
@@ -23,7 +23,7 @@ pub struct SovereignStorageConfig {
 
 #[derive(Serialize)]
 pub struct SovereignRunnerConfig {
-    pub genesis_height: u64,
+    pub start_height: u64,
     pub rpc_config: SovereignRpcConfig,
     pub da_polling_interval_ms: u64,
 }
