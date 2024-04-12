e12bb5de7290b4df5fa6c6a14654a1d9ba857638
Soares Chen <soares.chen@maybevoid.com>
2024-04-12 14:04:29 +0200

    Implement QueryCosmosConsensusStateFromAbci

--- crates/cosmos/cosmos-chain-components/src/impls/queries/consensus_state.rs
+++ crates/cosmos/cosmos-chain-components/src/impls/queries/consensus_state.rs
@@ -1,7 +1,9 @@
 use cgp_core::prelude::*;
 use cgp_core::CanRaiseError;
 use eyre::eyre;
-use hermes_relayer_components::chain::traits::queries::consensus_state::ConsensusStateQuerier;
+use hermes_relayer_components::chain::traits::queries::consensus_state::{
+    ConsensusStateBytesQuerier, ConsensusStateQuerier,
+};
 use hermes_relayer_components::chain::traits::types::consensus_state::HasConsensusStateType;
 use hermes_relayer_components::chain::traits::types::height::HasHeightType;
 use hermes_relayer_components::chain::traits::types::ibc::HasIbcChainTypes;
@@ -11,8 +13,42 @@ use ibc_relayer::consensus_state::AnyConsensusState;
 use ibc_relayer_types::core::ics24_host::identifier::ClientId;
 use ibc_relayer_types::Height;
 
+use crate::traits::abci_query::CanQueryAbci;
 use crate::traits::chain_handle::HasBlockingChainHandle;
 use crate::types::tendermint::TendermintConsensusState;
+
+pub struct QueryCosmosConsensusStateFromAbci;
+
+pub const IBC_QUERY_PATH: &str = "store/ibc/key";
+
+impl<Chain, Counterparty> ConsensusStateBytesQuerier<Chain, Counterparty>
+    for QueryCosmosConsensusStateFromAbci
+where
+    Chain: HasIbcChainTypes<Counterparty, ClientId = ClientId, Height = Height> + CanQueryAbci,
+    Counterparty: HasHeightType<Height = Height>,
+{
+    async fn query_consensus_state_bytes(
+        chain: &Chain,
+        client_id: &ClientId,
+        consensus_height: &Height,
+        query_height: &Height,
+    ) -> Result<Vec<u8>, Chain::Error> {
+        let revision_number = consensus_height.revision_number();
+        let revision_height = consensus_height.revision_height();
+        let consensus_state_path =
+            format!("clients/{client_id}/consensusStates/{revision_number}-{revision_height}");
+
+        let consensus_state_bytes = chain
+            .query_abci(
+                IBC_QUERY_PATH,
+                consensus_state_path.as_bytes(),
+                query_height,
+            )
+            .await?;
+
+        Ok(consensus_state_bytes)
+    }
+}
 
 pub struct QueryCosmosConsensusStateFromChainHandle;
 
