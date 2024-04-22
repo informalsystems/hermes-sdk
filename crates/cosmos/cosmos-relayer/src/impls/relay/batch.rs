29250a82f68d936b75ef4016359917f86f70cb87
Soares Chen <soares.chen@maybevoid.com>
2024-04-22 11:26:22 +0200

    Add provider trait for HasMessageBatchSender

--- crates/cosmos/cosmos-relayer/src/impls/relay/batch.rs
+++ crates/cosmos/cosmos-relayer/src/impls/relay/batch.rs
@@ -1,17 +1,18 @@
 use hermes_relayer_components::relay::traits::target::{DestinationTarget, SourceTarget};
-use hermes_relayer_components_extra::batch::traits::channel::HasMessageBatchSender;
+use hermes_relayer_components_extra::batch::traits::channel::MessageBatchSenderGetter;
 
 use crate::contexts::relay::CosmosRelay;
+use crate::impls::relay::component::CosmosRelayComponents;
 use crate::types::batch::CosmosBatchSender;
 
-impl HasMessageBatchSender<SourceTarget> for CosmosRelay {
-    fn get_batch_sender(&self) -> &CosmosBatchSender {
-        &self.src_chain_message_batch_sender
+impl MessageBatchSenderGetter<CosmosRelay, SourceTarget> for CosmosRelayComponents {
+    fn get_batch_sender(relay: &CosmosRelay) -> &CosmosBatchSender {
+        &relay.src_chain_message_batch_sender
     }
 }
 
-impl HasMessageBatchSender<DestinationTarget> for CosmosRelay {
-    fn get_batch_sender(&self) -> &CosmosBatchSender {
-        &self.dst_chain_message_batch_sender
+impl MessageBatchSenderGetter<CosmosRelay, DestinationTarget> for CosmosRelayComponents {
+    fn get_batch_sender(relay: &CosmosRelay) -> &CosmosBatchSender {
+        &relay.dst_chain_message_batch_sender
     }
 }
