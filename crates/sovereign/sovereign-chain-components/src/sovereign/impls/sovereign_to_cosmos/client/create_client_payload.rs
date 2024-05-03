a44b743c71db082f549b909dced7c023abc63cc0
Ranadeep Biswas <mail@rnbguy.at>
2024-05-03 19:36:52 +0200

    finish sov client create

--- crates/sovereign/sovereign-chain-components/src/sovereign/impls/sovereign_to_cosmos/client/create_client_payload.rs
+++ crates/sovereign/sovereign-chain-components/src/sovereign/impls/sovereign_to_cosmos/client/create_client_payload.rs
@@ -8,11 +8,12 @@ use hermes_relayer_components::chain::traits::types::create_client::{
 };
 use ibc::core::client::types::Height;
 use ibc::core::host::types::identifiers::ChainId;
-use ibc::primitives::Timestamp;
 use ibc_relayer::chain::handle::ChainHandle;
-use sov_celestia_client::types::client_state::test_util::{
-    dummy_sov_client_state, dummy_sov_consensus_state,
-};
+use ibc_relayer::chain::requests::{QueryHeight, QueryHostConsensusStateRequest};
+use ibc_relayer::consensus_state::AnyConsensusState;
+use sov_celestia_client::types::client_state::test_util::dummy_sov_client_state;
+use sov_celestia_client::types::consensus_state::{SovTmConsensusState, TmConsensusParams};
+use sov_celestia_client::types::sovereign::SovereignConsensusParams;
 
 use crate::sovereign::traits::chain::data_chain::{HasDataChain, HasDataChainType};
 use crate::sovereign::types::payloads::client::{
@@ -61,8 +62,32 @@ where
 
         // returns with genesis_da_height: Height(0, 3)
         let client_state = dummy_sov_client_state(chain_id, latest_height);
-        let tm = Timestamp::now();
-        let consensus_state = dummy_sov_consensus_state(tm);
+
+        let host_consensus_state_query = QueryHostConsensusStateRequest {
+            height: QueryHeight::Specific(height),
+        };
+
+        let any_consensus_state = data_chain
+            .with_blocking_chain_handle(move |chain_handle| {
+                Ok(chain_handle
+                    .query_host_consensus_state(host_consensus_state_query)
+                    .unwrap())
+            })
+            .await
+            .unwrap();
+
+        let AnyConsensusState::Tendermint(tm_consensus_state) = any_consensus_state else {
+            panic!("Expected Tendermint consensus state");
+        };
+
+        let tendermint_params = TmConsensusParams::new(
+            tm_consensus_state.timestamp,
+            tm_consensus_state.next_validators_hash,
+        );
+
+        let sovereign_params = SovereignConsensusParams::new(vec![0].into());
+
+        let consensus_state = SovTmConsensusState::new(sovereign_params, tendermint_params);
 
         let code_hash = create_client_options.code_hash.clone();
 
