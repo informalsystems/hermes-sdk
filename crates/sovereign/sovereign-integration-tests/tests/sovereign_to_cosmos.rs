c05b3bf0a768272b25d5a79b44a5ef44a6993b48
Soares Chen <soares.chen@maybevoid.com>
2024-05-06 14:59:42 +0200

    No need to create Celestia client on Cosmos

--- crates/sovereign/sovereign-integration-tests/tests/sovereign_to_cosmos.rs
+++ crates/sovereign/sovereign-integration-tests/tests/sovereign_to_cosmos.rs
@@ -19,9 +19,9 @@ use hermes_relayer_components::chain::traits::message_builders::update_client::C
 use hermes_relayer_components::chain::traits::payload_builders::connection_handshake::CanBuildConnectionHandshakePayloads;
 use hermes_relayer_components::chain::traits::payload_builders::create_client::CanBuildCreateClientPayload;
 use hermes_relayer_components::chain::traits::payload_builders::update_client::CanBuildUpdateClientPayload;
+use hermes_relayer_components::chain::traits::queries::chain_status::CanQueryChainHeight;
 use hermes_relayer_components::chain::traits::queries::client_state::CanQueryClientStateWithLatestHeight;
 use hermes_relayer_components::chain::traits::send_message::CanSendSingleMessage;
-use hermes_relayer_components::chain::traits::types::create_client::HasCreateClientEvent;
 use hermes_runtime::types::runtime::HermesRuntime;
 use hermes_sovereign_chain_components::sovereign::types::payloads::client::SovereignCreateClientOptions;
 use hermes_sovereign_integration_tests::contexts::cosmos_bootstrap::CosmosWithWasmClientBootstrap;
@@ -32,9 +32,6 @@ use hermes_sovereign_test_components::bootstrap::traits::bootstrap_rollup::CanBo
 use hermes_test_components::bootstrap::traits::chain::CanBootstrapChain;
 use hermes_test_components::chain_driver::traits::types::chain::HasChain;
 use hermes_wasm_client_components::contexts::wasm_counterparty::WasmCounterparty;
-use ibc_relayer::chain::client::ClientSettings;
-use ibc_relayer::chain::cosmos::client::Settings;
-use ibc_relayer_types::core::ics02_client::trust_threshold::TrustThreshold;
 use ibc_relayer_types::core::ics03_connection::version::Version;
 use ibc_relayer_types::core::ics24_host::identifier::ClientId;
 use sha2::{Digest, Sha256};
@@ -43,7 +40,7 @@ use tracing::info;
 
 #[tracing::instrument]
 #[test]
-pub fn test_create_sovereign_client_on_cosmos() -> Result<(), Error> {
+pub fn test_sovereign_to_cosmos() -> Result<(), Error> {
     tracing_subscriber::fmt()
         .with_max_level(tracing::Level::INFO)
         .init();
@@ -94,12 +91,6 @@ pub fn test_create_sovereign_client_on_cosmos() -> Result<(), Error> {
         account_prefix: "sov".into(),
     };
 
-    let create_client_settings = ClientSettings::Tendermint(Settings {
-        max_clock_drift: Duration::from_secs(40),
-        trusting_period: None,
-        trust_threshold: TrustThreshold::ONE_THIRD,
-    });
-
     let wasm_client_bytes = std::fs::read(&wasm_client_code_path)?;
 
     let wasm_code_hash: [u8; 32] = {
@@ -125,13 +116,13 @@ pub fn test_create_sovereign_client_on_cosmos() -> Result<(), Error> {
             .bootstrap_rollup(&celestia_chain_driver, &bridge_driver, "test-rollup")
             .await?;
 
+        let rollup = rollup_driver.rollup;
+
         let sovereign_chain = SovereignChain {
             runtime: runtime.clone(),
             data_chain: celestia_chain.clone(),
-            rollup: rollup_driver.rollup,
+            rollup: rollup.clone(),
         };
-
-        // FIXME: The test would fail if the Wasm client is initialized too early
 
         let cosmos_chain_driver = cosmos_bootstrap.bootstrap_chain("cosmos-1").await?;
 
@@ -150,41 +141,21 @@ pub fn test_create_sovereign_client_on_cosmos() -> Result<(), Error> {
 
         let _events = cosmos_chain.send_message(create_client_message).await?;
 
-        let wasm_client_id = ClientId::from_str("08-wasm-0").map_err(|e| eyre!("Failed to create a Client ID from string '08-wasm-0': {e}"))?;
+        let wasm_client_id = ClientId::from_str("08-wasm-0")?;
 
         let sovereign_client_state = <CosmosChain as CanQueryClientStateWithLatestHeight<SovereignChain>>::query_client_state_with_latest_height(cosmos_chain, &wasm_client_id).await?;
 
-        // Create Celestia client (DA client) on Cosmos chain
-        let create_celestia_client_payload = <CosmosChain as CanBuildCreateClientPayload<CosmosChain>>::build_create_client_payload(
-            celestia_chain,
-            &create_client_settings
-        ).await?;
-
-        let create_celestia_client_message = <CosmosChain as CanBuildCreateClientMessage<CosmosChain>>::build_create_client_message(
-            cosmos_chain,
-            create_celestia_client_payload,
-        ).await?;
-
-        let events = cosmos_chain.send_message(create_celestia_client_message).await?;
-
-        let create_client_event = events.into_iter()
-            .find_map(<CosmosChain as HasCreateClientEvent<CosmosChain>>::try_extract_create_client_event)
-            .ok_or_else(|| eyre!("Could not extract Celestia create client event"))?;
-
-        let celestia_client_id = create_client_event.client_id;
-
         let wasm_client_state = <CosmosChain as CanQueryClientStateWithLatestHeight<WasmCounterparty>>::query_client_state_with_latest_height(cosmos_chain, &wasm_client_id).await?;
 
-        let celestia_client_state = <CosmosChain as CanQueryClientStateWithLatestHeight<CosmosChain>>::query_client_state_with_latest_height(cosmos_chain, &celestia_client_id).await?;
+        let trusted_height = RollupHeight { slot_number: wasm_client_state.latest_height.revision_height() };
 
-        let dummy_trusted_height = RollupHeight { slot_number: wasm_client_state.latest_height.revision_height() as u64 };
-        let dummy_target_height = RollupHeight { slot_number: (celestia_client_state.latest_height.revision_height()) as u64 };
+        let target_height = rollup.query_chain_height().await?;
 
         // Update Sovereign client state
         let update_client_payload = <SovereignChain as CanBuildUpdateClientPayload<CosmosChain>>::build_update_client_payload(
             &sovereign_chain,
-            &dummy_trusted_height,
-            &dummy_target_height,
+            &trusted_height,
+            &target_height,
             sovereign_client_state
         ).await?;
 
