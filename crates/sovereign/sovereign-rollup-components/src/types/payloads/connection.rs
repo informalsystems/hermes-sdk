883251c6334180fb1f8af5c221396dfb425f396a
Ranadeep Biswas <mail@rnbguy.at>
2024-05-06 18:56:17 +0200

    update SovereignInitConnectionOptions

--- crates/sovereign/sovereign-rollup-components/src/types/payloads/connection.rs
+++ crates/sovereign/sovereign-rollup-components/src/types/payloads/connection.rs
@@ -1,5 +1,10 @@
+use ibc_relayer_types::core::ics03_connection::version::Version;
+use std::time::Duration;
+
 pub struct SovereignInitConnectionOptions {
     // TODO: fill in fields
+    pub version: Version,
+    pub delay_period: Duration,
 }
 
 pub struct SovereignConnectionOpenInitRollupPayload {
