14f36f47b51d38b5d07805fd88289bd55a43bab1
Soares Chen <soares.chen@maybevoid.com>
2024-05-10 14:36:54 +0200

    Need to increment proof height by 1

--- crates/cosmos/cosmos-chain-components/src/impls/connection/connection_handshake_payload.rs
+++ crates/cosmos/cosmos-chain-components/src/impls/connection/connection_handshake_payload.rs
@@ -1,5 +1,4 @@
 use cgp_core::CanRaiseError;
-use eyre::eyre;
 use hermes_encoding_components::traits::convert::CanConvert;
 use hermes_encoding_components::traits::has_encoding::HasDefaultEncoding;
 use hermes_relayer_components::chain::traits::commitment_prefix::HasIbcCommitmentPrefix;
@@ -13,18 +12,19 @@ use hermes_relayer_components::chain::traits::types::client_state::{
     HasClientStateFields, HasClientStateType,
 };
 use hermes_relayer_components::chain::traits::types::connection::HasConnectionHandshakePayloadTypes;
-use hermes_relayer_components::chain::traits::types::height::HasHeightFields;
+use hermes_relayer_components::chain::traits::types::height::{
+    CanIncrementHeight, HasHeightFields,
+};
 use hermes_relayer_components::chain::traits::types::ibc::HasIbcChainTypes;
 use hermes_relayer_components::chain::traits::types::proof::HasCommitmentProofType;
 use ibc_relayer::chain::handle::ChainHandle;
 use ibc_relayer::client_state::AnyClientState;
 use ibc_relayer::connection::ConnectionMsgType;
+use ibc_relayer_types::core::ics02_client::error::Error as Ics02Error;
 use ibc_relayer_types::core::ics03_connection::connection::ConnectionEnd;
 use ibc_relayer_types::core::ics24_host::identifier::{ClientId, ConnectionId};
 use ibc_relayer_types::Height;
 use prost_types::Any;
-use tonic::metadata::errors::InvalidMetadataValue;
-use tonic::transport::Error as TransportError;
 
 use crate::traits::chain_handle::HasBlockingChainHandle;
 use crate::traits::grpc_address::HasGrpcAddress;
@@ -52,6 +52,7 @@ where
         > + HasClientStateType<Counterparty>
         + HasIbcCommitmentPrefix<CommitmentPrefix = Vec<u8>>
         + HasCommitmentProofType<CommitmentProof = Vec<u8>>
+        + CanIncrementHeight
         + CanQueryConnectionEnd<Counterparty, ConnectionEnd = ConnectionEnd>
         + CanQueryConnectionEndWithProofs<Counterparty, ConnectionEnd = ConnectionEnd>
         + CanQueryClientStateWithProofs<Counterparty>
@@ -59,9 +60,8 @@ where
         + HasGrpcAddress
         + HasBlockingChainHandle
         + CanRaiseError<Encoding::Error>
-        + CanRaiseError<TransportError>
-        + CanRaiseError<InvalidMetadataValue>
-        + CanRaiseError<eyre::Report>,
+        + CanRaiseError<Ics02Error>
+        + CanRaiseError<&'static str>,
     Counterparty:
         HasClientStateFields<Chain> + HasDefaultEncoding<Encoding = Encoding> + HasHeightFields,
     Encoding: CanConvert<Counterparty::ClientState, Any>,
@@ -104,20 +104,24 @@ where
             .query_raw_consensus_state_with_proofs(client_id, &consensus_state_height, height)
             .await?;
 
+        let update_height = Chain::increment_height(height)?;
+
+        let proof_consensus_height = Height::new(
+            Counterparty::revision_number(&consensus_state_height),
+            Counterparty::revision_height(&consensus_state_height),
+        )
+        .map_err(Chain::raise_error)?;
+
         let payload = CosmosConnectionOpenTryPayload {
             commitment_prefix,
             client_state: client_state_any,
             versions,
             delay_period,
-            update_height: height.clone(),
+            update_height,
             proof_init: connection_proofs,
             proof_client: client_state_proofs,
             proof_consensus: consensus_state_proofs,
-            proof_consensus_height: Height::new(
-                Counterparty::revision_number(&consensus_state_height),
-                Counterparty::revision_height(&consensus_state_height),
-            )
-            .unwrap(),
+            proof_consensus_height,
         };
 
         Ok(payload)
@@ -154,8 +158,8 @@ where
                     )
                     .map_err(Chain::raise_error)?;
 
-                let any_client_state = m_client_state
-                    .ok_or_else(|| Chain::raise_error(eyre!("expect some client state")))?;
+                let any_client_state =
+                    m_client_state.ok_or_else(|| Chain::raise_error("expect some client state"))?;
 
                 let client_state = match any_client_state {
                     AnyClientState::Tendermint(client_state) => client_state,
@@ -163,12 +167,12 @@ where
 
                 let proof_client = proofs
                     .client_proof()
-                    .ok_or_else(|| Chain::raise_error(eyre!("expect non empty client proof")))?
+                    .ok_or_else(|| Chain::raise_error("expect non empty client proof"))?
                     .clone();
 
                 let proof_consensus = proofs
                     .consensus_proof()
-                    .ok_or_else(|| Chain::raise_error(eyre!("expect non empty consensus proof")))?
+                    .ok_or_else(|| Chain::raise_error("expect non empty consensus proof"))?
                     .clone();
 
                 let payload = CosmosConnectionOpenAckPayload {
