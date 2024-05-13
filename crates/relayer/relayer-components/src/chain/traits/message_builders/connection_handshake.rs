adc1ff919b4178e18911ad76ba5696d7dfd08f17
Soares Chen <soares.chen@maybevoid.com>
2024-05-13 10:09:59 +0200

    Split out connection handshake message builders

--- crates/relayer/relayer-components/src/chain/traits/message_builders/connection_handshake.rs
+++ crates/relayer/relayer-components/src/chain/traits/message_builders/connection_handshake.rs
@@ -1,9 +1,72 @@
 use cgp_core::prelude::*;
 
 use crate::chain::traits::types::connection::{
-    HasConnectionHandshakePayloadTypes, HasInitConnectionOptionsType,
+    HasConnectionHandshakePayloadTypes, HasConnectionOpenAckPayloadType,
+    HasConnectionOpenConfirmPayloadType, HasConnectionOpenInitPayloadType,
+    HasConnectionOpenTryPayloadType, HasInitConnectionOptionsType,
 };
 use crate::chain::traits::types::ibc::HasIbcChainTypes;
+
+#[derive_component(ConnectionOpenInitMessageBuilderComponent, ConnectionOpenInitMessageBuilder<Chain>)]
+#[async_trait]
+pub trait CanBuildConnectionOpenInitMessages<Counterparty>:
+    HasInitConnectionOptionsType<Counterparty> + HasIbcChainTypes<Counterparty> + HasErrorType
+where
+    Counterparty: HasConnectionOpenInitPayloadType<Self> + HasIbcChainTypes<Self>,
+{
+    async fn build_connection_open_init_message(
+        &self,
+        client_id: &Self::ClientId,
+        counterparty_client_id: &Counterparty::ClientId,
+        init_connection_options: &Self::InitConnectionOptions,
+        counterparty_payload: Counterparty::ConnectionOpenInitPayload,
+    ) -> Result<Self::Message, Self::Error>;
+}
+
+#[derive_component(ConnectionOpenTryMessageBuilderComponent, ConnectionOpenTryMessageBuilder<Chain>)]
+#[async_trait]
+pub trait CanBuildConnectionOpenTryMessages<Counterparty>:
+    HasIbcChainTypes<Counterparty> + HasErrorType
+where
+    Counterparty: HasConnectionOpenTryPayloadType<Self> + HasIbcChainTypes<Self>,
+{
+    async fn build_connection_open_try_message(
+        &self,
+        client_id: &Self::ClientId,
+        counterparty_client_id: &Counterparty::ClientId,
+        counterparty_connection_id: &Counterparty::ConnectionId,
+        counterparty_payload: Counterparty::ConnectionOpenTryPayload,
+    ) -> Result<Self::Message, Self::Error>;
+}
+
+#[derive_component(ConnectionOpenAckMessageBuilderComponent, ConnectionOpenAckMessageBuilder<Chain>)]
+#[async_trait]
+pub trait CanBuildConnectionOpenAckMessages<Counterparty>:
+    HasIbcChainTypes<Counterparty> + HasErrorType
+where
+    Counterparty: HasConnectionOpenAckPayloadType<Self> + HasIbcChainTypes<Self>,
+{
+    async fn build_connection_open_ack_message(
+        &self,
+        connection_id: &Self::ConnectionId,
+        counterparty_connection_id: &Counterparty::ConnectionId,
+        counterparty_payload: Counterparty::ConnectionOpenAckPayload,
+    ) -> Result<Self::Message, Self::Error>;
+}
+
+#[derive_component(ConnectionOpenConfirmMessageBuilderComponent, ConnectionOpenConfirmMessageBuilder<Chain>)]
+#[async_trait]
+pub trait CanBuildConnectionOpenConfirmMessages<Counterparty>:
+    HasIbcChainTypes<Counterparty> + HasErrorType
+where
+    Counterparty: HasConnectionOpenConfirmPayloadType<Self> + HasIbcChainTypes<Self>,
+{
+    async fn build_connection_open_confirm_message(
+        &self,
+        connection_id: &Self::ConnectionId,
+        counterparty_payload: Counterparty::ConnectionOpenConfirmPayload,
+    ) -> Result<Self::Message, Self::Error>;
+}
 
 #[derive_component(ConnectionHandshakeMessageBuilderComponent, ConnectionHandshakeMessageBuilder<Chain>)]
 #[async_trait]
