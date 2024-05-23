5d8c0b29c3034cec28044abe9d2cf0c6144c16af
Soares Chen <soares.chen@maybevoid.com>
2024-05-23 21:12:56 +0200

    Use HasSendPacketEvent in QueryCosmosSendPacket

--- crates/cosmos/cosmos-chain-components/src/methods/event.rs
+++ crates/cosmos/cosmos-chain-components/src/methods/event.rs
@@ -1,23 +1,9 @@
 use alloc::sync::Arc;
 
 use ibc_relayer::event::extract_packet_and_write_ack_from_tx;
-use ibc_relayer_types::core::ics04_channel::events::{SendPacket, WriteAcknowledgement};
+use ibc_relayer_types::core::ics04_channel::events::WriteAcknowledgement;
 use ibc_relayer_types::events::IbcEventType;
 use tendermint::abci::Event as AbciEvent;
-
-pub fn try_extract_send_packet_event(event: &Arc<AbciEvent>) -> Option<SendPacket> {
-    let event_type = event.kind.parse().ok()?;
-
-    if let IbcEventType::SendPacket = event_type {
-        let (packet, _) = extract_packet_and_write_ack_from_tx(event).ok()?;
-
-        let send_packet_event = SendPacket { packet };
-
-        Some(send_packet_event)
-    } else {
-        None
-    }
-}
 
 pub fn try_extract_write_ack_event(event: &Arc<AbciEvent>) -> Option<WriteAcknowledgement> {
     if let IbcEventType::WriteAck = event.kind.parse().ok()? {
