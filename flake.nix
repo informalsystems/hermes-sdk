fdb18a8871cff9857c0256a69035e7bac1982c17
Soares Chen <soares.chen@maybevoid.com>
2024-08-26 17:17:58 +0200

    Use back Cosmos.nix main branch

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     rust-overlay.url = github:oxalica/rust-overlay;
     flake-utils.url = github:numtide/flake-utils;
 
-    cosmos-nix.url = github:informalsystems/cosmos.nix/soares/ibc-go-wasm;
+    cosmos-nix.url = github:informalsystems/cosmos.nix;
 
     cosmwasm-ibc-src = {
       url = github:informalsystems/cosmwasm-ibc;
