78a5c7ea91275d271588e5ef71cdd7936aff8e5e
Soares Chen <soares.chen@maybevoid.com>
2024-09-13 08:16:37 +0000

    Move proto field encoders to proto_field submodule

--- crates/encoding/protobuf-encoding-components/src/impls/encode_mut/mod.rs
+++ crates/encoding/protobuf-encoding-components/src/impls/encode_mut/mod.rs
@@ -2,6 +2,3 @@ pub mod chunk;
 pub mod message;
 pub mod option;
 pub mod proto_field;
-pub mod proto_field_len;
-pub mod proto_field_required;
-pub mod proto_optional;
---
+++ crates/encoding/protobuf-encoding-components/src/impls/encode_mut/proto_field/mod.rs
@@ -0,0 +1,4 @@
+pub mod decode_optional;
+pub mod decode_required;
+pub mod encode;
+pub mod encode_len;
