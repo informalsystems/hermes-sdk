1bbbddf680870e2360161c865a8a1359b0f82260
Soares Chen <soares.chen@maybevoid.com>
2024-09-13 14:18:50 +0000

    Simplify delegation via WasmEncodingComponents

--- crates/cosmos/cosmos-wasm-relayer/src/encoding/components.rs
+++ crates/cosmos/cosmos-wasm-relayer/src/encoding/components.rs
@@ -3,8 +3,6 @@ pub use hermes_cosmos_chain_components::encoding::components::{
     DecodeBufferTypeComponent, EncodeBufferTypeComponent,
 };
 use hermes_encoding_components::impls::delegate::DelegateEncoding;
-use hermes_encoding_components::impls::encoded::ProvideEncodedBytes;
-use hermes_encoding_components::impls::schema::ProvideStringSchema;
 pub use hermes_encoding_components::traits::convert::ConverterComponent;
 pub use hermes_encoding_components::traits::decode::DecoderComponent;
 pub use hermes_encoding_components::traits::decode_mut::MutDecoderComponent;
@@ -13,9 +11,8 @@ pub use hermes_encoding_components::traits::encode_mut::MutEncoderComponent;
 pub use hermes_encoding_components::traits::schema::SchemaGetterComponent;
 pub use hermes_encoding_components::traits::types::encoded::EncodedTypeComponent;
 pub use hermes_encoding_components::traits::types::schema::SchemaTypeComponent;
-use hermes_protobuf_encoding_components::impls::types::decode_buffer::ProvideProtoChunksDecodeBuffer;
-use hermes_protobuf_encoding_components::impls::types::encode_buffer::ProvideBytesEncodeBuffer;
 pub use hermes_protobuf_encoding_components::traits::length::EncodedLengthGetterComponent;
+use hermes_wasm_client_components::impls::encoding::components::WasmEncodingComponents;
 
 use crate::encoding::convert::WasmCosmosConverterComponents;
 use crate::encoding::encode::WasmCosmosEncoderComponents;
@@ -24,14 +21,13 @@ use crate::encoding::type_url::WasmCosmosTypeUrlSchemas;
 
 define_components! {
     WasmCosmosEncodingComponents {
-        EncodedTypeComponent:
-            ProvideEncodedBytes,
-        EncodeBufferTypeComponent:
-            ProvideBytesEncodeBuffer,
-        DecodeBufferTypeComponent:
-            ProvideProtoChunksDecodeBuffer,
-        SchemaTypeComponent:
-            ProvideStringSchema,
+        [
+            EncodedTypeComponent,
+            EncodeBufferTypeComponent,
+            DecodeBufferTypeComponent,
+            SchemaTypeComponent,
+        ]:
+            WasmEncodingComponents,
         ConverterComponent:
             DelegateEncoding<WasmCosmosConverterComponents>,
         [
--- crates/cosmos/cosmos-wasm-relayer/src/encoding/encode_mut.rs
+++ crates/cosmos/cosmos-wasm-relayer/src/encoding/encode_mut.rs
@@ -1,7 +1,7 @@
 use cgp::prelude::*;
-use hermes_cosmos_chain_components::encoding::components::CosmosEncodingComponents;
 use hermes_protobuf_encoding_components::types::strategy::ViaProtobuf;
-use hermes_wasm_client_components::types::client_state::{EncodeWasmClientState, WasmClientState};
+use hermes_wasm_client_components::impls::encoding::components::WasmEncodingComponents;
+use hermes_wasm_client_components::types::client_state::WasmClientState;
 use ibc::core::client::types::Height;
 
 pub struct WasmCosmosEncodeMutComponents;
@@ -10,9 +10,7 @@ delegate_components! {
     WasmCosmosEncodeMutComponents {
         [
             (ViaProtobuf, Height),
-        ]: CosmosEncodingComponents,
-
-        (ViaProtobuf, WasmClientState):
-            EncodeWasmClientState,
+            (ViaProtobuf, WasmClientState),
+        ]: WasmEncodingComponents,
     }
 }
