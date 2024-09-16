1db859f076dbf29b8b0dd349686d1b78e7a914ee
Soares Chen <soares.chen@maybevoid.com>
2024-09-16 11:55:38 +0000

    Simplify encoder implementation for Any

--- crates/encoding/protobuf-encoding-components/src/impls/encode_mut/any.rs
+++ crates/encoding/protobuf-encoding-components/src/impls/encode_mut/any.rs
@@ -1,8 +1,14 @@
-use cgp::prelude::HasErrorType;
-use hermes_encoding_components::traits::decode_mut::MutDecoder;
-use hermes_encoding_components::traits::encode_mut::MutEncoder;
-use hermes_encoding_components::traits::types::decode_buffer::HasDecodeBufferType;
-use hermes_encoding_components::traits::types::encode_buffer::HasEncodeBufferType;
+use core::marker::PhantomData;
+
+use cgp::prelude::*;
+use hermes_encoding_components::impls::encode_mut::combine::CombineEncoders;
+use hermes_encoding_components::impls::encode_mut::field::EncodeField;
+use hermes_encoding_components::impls::encode_mut::from::DecodeFrom;
+use hermes_encoding_components::traits::decode_mut::MutDecoderComponent;
+use hermes_encoding_components::traits::encode_mut::MutEncoderComponent;
+use hermes_encoding_components::traits::field::FieldGetter;
+use hermes_encoding_components::traits::transform::Transformer;
+use hermes_encoding_components::HList;
 use prost_types::Any;
 
 use crate::impls::encode_mut::proto_field::bytes::EncodeByteField;
@@ -10,37 +16,53 @@ use crate::impls::encode_mut::proto_field::string::EncodeStringField;
 
 pub struct EncodeAny;
 
-impl<Encoding, Strategy> MutEncoder<Encoding, Strategy, Any> for EncodeAny
-where
-    Encoding: HasEncodeBufferType + HasErrorType,
-    EncodeStringField<1>: MutEncoder<Encoding, Strategy, String>,
-    EncodeByteField<2>: MutEncoder<Encoding, Strategy, Vec<u8>>,
-{
-    fn encode_mut(
-        encoding: &Encoding,
-        any: &Any,
-        buffer: &mut Encoding::EncodeBuffer,
-    ) -> Result<(), Encoding::Error> {
-        <EncodeStringField<1>>::encode_mut(encoding, &any.type_url, buffer)?;
-        <EncodeByteField<2>>::encode_mut(encoding, &any.value, buffer)?;
+pub struct GetAnyField<Tag>(pub PhantomData<Tag>);
 
-        Ok(())
+delegate_components! {
+    EncodeAny {
+        MutEncoderComponent:
+            CombineEncoders<HList![
+                EncodeField<
+                    GetAnyField<symbol!("type_url")>,
+                    EncodeStringField<1>,
+                >,
+                EncodeField<
+                    GetAnyField<symbol!("value")>,
+                    EncodeByteField<2>,
+                >,
+            ]>,
+        MutDecoderComponent: DecodeFrom<
+            Self,
+            CombineEncoders<HList![
+                EncodeStringField<1>,
+                EncodeByteField<2>,
+            ]>
+        >,
     }
 }
 
-impl<Encoding, Strategy> MutDecoder<Encoding, Strategy, Any> for EncodeAny
-where
-    Encoding: HasDecodeBufferType + HasErrorType,
-    EncodeStringField<1>: MutDecoder<Encoding, Strategy, String>,
-    EncodeByteField<2>: MutDecoder<Encoding, Strategy, Vec<u8>>,
-{
-    fn decode_mut<'a>(
-        encoding: &Encoding,
-        buffer: &mut Encoding::DecodeBuffer<'a>,
-    ) -> Result<Any, Encoding::Error> {
-        let type_url = <EncodeStringField<1>>::decode_mut(encoding, buffer)?;
-        let value = <EncodeByteField<2>>::decode_mut(encoding, buffer)?;
+impl FieldGetter<Any> for GetAnyField<symbol!("type_url")> {
+    type Field = String;
 
-        Ok(Any { type_url, value })
+    fn get_field(any: &Any) -> &String {
+        &any.type_url
+    }
+}
+
+impl FieldGetter<Any> for GetAnyField<symbol!("value")> {
+    type Field = Vec<u8>;
+
+    fn get_field(any: &Any) -> &Vec<u8> {
+        &any.value
+    }
+}
+
+impl Transformer for EncodeAny {
+    type From = HList![String, Vec<u8>];
+
+    type To = Any;
+
+    fn transform(HList![type_url, value]: Self::From) -> Self::To {
+        Any { type_url, value }
     }
 }
