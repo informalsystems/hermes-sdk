f02b2dfe277508072b742df3ef61be15fa263310
Soares Chen <soares.chen@maybevoid.com>
2024-09-18 14:37:58 +0000

    Add encode/decode via ClientMessage converters

---
+++ crates/wasm/wasm-encoding-components/src/impls/convert/client_message.rs
@@ -0,0 +1,41 @@
+use hermes_encoding_components::traits::convert::{CanConvert, Converter};
+use hermes_encoding_components::traits::decode::CanDecode;
+use hermes_encoding_components::traits::encode::CanEncode;
+use hermes_encoding_components::traits::types::encoded::HasEncodedType;
+use hermes_protobuf_encoding_components::types::strategy::ViaAny;
+use ibc::clients::wasm_types::client_message::ClientMessage;
+use ibc::primitives::proto::Any;
+
+pub struct EncodeViaClientMessage;
+
+impl<Encoding, Value> Converter<Encoding, Value, Any> for EncodeViaClientMessage
+where
+    Encoding: HasEncodedType<Encoded = Vec<u8>>
+        + CanEncode<ViaAny, Value>
+        + CanConvert<ClientMessage, Any>,
+{
+    fn convert(encoding: &Encoding, value: &Value) -> Result<Any, Encoding::Error> {
+        let data = encoding.encode(value)?;
+
+        let client_message = ClientMessage { data };
+
+        encoding.convert(&client_message)
+    }
+}
+
+pub struct DecodeViaClientMessage;
+
+impl<Encoding, Value> Converter<Encoding, Any, Value> for DecodeViaClientMessage
+where
+    Encoding: HasEncodedType<Encoded = Vec<u8>>
+        + CanDecode<ViaAny, Value>
+        + CanConvert<Any, ClientMessage>,
+{
+    fn convert(encoding: &Encoding, any: &Any) -> Result<Value, Encoding::Error> {
+        let message = encoding.convert(any)?;
+
+        let value = encoding.decode(&message.data)?;
+
+        Ok(value)
+    }
+}
