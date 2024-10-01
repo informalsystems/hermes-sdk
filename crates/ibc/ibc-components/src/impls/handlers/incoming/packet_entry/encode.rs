698d68f5a6dfbf0300be5e0ac43cfd97da582046
Soares Chen <soares.chen@maybevoid.com>
2024-10-01 12:30:44 +0000

    Implement EncodeAndHandlePacketEntry

--- crates/ibc/ibc-components/src/impls/handlers/incoming/packet_entry/encode.rs
+++ crates/ibc/ibc-components/src/impls/handlers/incoming/packet_entry/encode.rs
@@ -1,8 +1,14 @@
 use core::marker::PhantomData;
 
-use cgp::prelude::HasErrorType;
+use cgp::core::error::ErrorOf;
+use cgp::core::Async;
+use cgp::prelude::CanRaiseError;
+use hermes_encoding_components::traits::convert::CanConvert;
+use hermes_encoding_components::traits::has_encoding::{HasDefaultEncoding, HasEncoding};
 
-use crate::traits::handlers::incoming::packet_entry::IncomingPacketEntryHandler;
+use crate::traits::handlers::incoming::packet_entry::{
+    CanHandleIncomingPacketEntry, IncomingPacketEntryHandler,
+};
 use crate::traits::types::packet::ack::HasPacketAckType;
 use crate::traits::types::packet::data::HasPacketDataType;
 use crate::traits::types::packet::entry::HasPacketEntryHeaderType;
@@ -10,20 +16,41 @@ use crate::traits::types::packet::header::HasPacketHeaderType;
 
 pub struct EncodeAndHandlePacketEntry<InApp>(pub PhantomData<InApp>);
 
-impl<Chain, Counterparty, App, InApp> IncomingPacketEntryHandler<Chain, Counterparty, App>
-    for EncodeAndHandlePacketEntry<InApp>
+impl<Chain, Counterparty, App, InApp, RawPacketData, PacketData, RawPacketAck, PacketAck>
+    IncomingPacketEntryHandler<Chain, Counterparty, App> for EncodeAndHandlePacketEntry<InApp>
 where
-    Chain: HasErrorType + HasPacketAckType<Counterparty, App>,
+    Chain: HasPacketAckType<Counterparty, App, PacketAck = RawPacketAck>
+        + HasPacketAckType<Counterparty, InApp, PacketAck = PacketAck>
+        + CanHandleIncomingPacketEntry<Counterparty, InApp>
+        + HasEncoding<App>
+        + CanRaiseError<ErrorOf<Chain::Encoding>>
+        + CanRaiseError<ErrorOf<Counterparty::Encoding>>,
     Counterparty: HasPacketHeaderType<Chain>
         + HasPacketEntryHeaderType<Chain>
-        + HasPacketDataType<Chain, App>,
+        + HasPacketDataType<Chain, App, PacketData = RawPacketData>
+        + HasPacketDataType<Chain, InApp, PacketData = PacketData>
+        + HasDefaultEncoding<App>,
+    Chain::Encoding: CanConvert<PacketAck, RawPacketAck>,
+    Counterparty::Encoding: CanConvert<RawPacketData, PacketData>,
+    PacketData: Async,
+    RawPacketData: Async,
 {
     async fn handle_incoming_packet_entry(
-        _chain: &Chain,
-        _packet_header: &Counterparty::PacketHeader,
-        _entry_header: &Counterparty::PacketEntryHeader,
-        _entry_data: &Counterparty::PacketData,
-    ) -> Result<Chain::PacketAck, Chain::Error> {
-        todo!()
+        chain: &Chain,
+        packet_header: &Counterparty::PacketHeader,
+        entry_header: &Counterparty::PacketEntryHeader,
+        raw_packet_data: &RawPacketData,
+    ) -> Result<RawPacketAck, Chain::Error> {
+        let packet_data = Counterparty::default_encoding()
+            .convert(raw_packet_data)
+            .map_err(Chain::raise_error)?;
+
+        let ack = chain
+            .handle_incoming_packet_entry(packet_header, entry_header, &packet_data)
+            .await?;
+
+        let raw_ack = chain.encoding().convert(&ack).map_err(Chain::raise_error)?;
+
+        Ok(raw_ack)
     }
 }
