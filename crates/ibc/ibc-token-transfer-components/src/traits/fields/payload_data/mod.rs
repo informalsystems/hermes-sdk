7b7cc83fb00002db6e939c0c7e92d850529d788c
Soares Chen <soares.chen@maybevoid.com>
2024-10-03 14:00:45 +0000

    Implement HandleIncomingMintTransfer

--- crates/ibc/ibc-token-transfer-components/src/traits/fields/payload_data/mod.rs
+++ crates/ibc/ibc-token-transfer-components/src/traits/fields/payload_data/mod.rs
@@ -1,3 +1,5 @@
 pub mod amount;
+pub mod mint_amount;
 pub mod receiver;
 pub mod sender;
+pub mod unescrow_amount;
---
+++ crates/ibc/ibc-token-transfer-components/src/traits/fields/payload_data/unescrow_amount.rs
@@ -0,0 +1,14 @@
+use cgp::prelude::*;
+use hermes_chain_type_components::traits::types::amount::HasAmountType;
+use hermes_ibc_components::traits::types::payload::data::HasPayloadDataType;
+
+#[derive_component(PayloadUnescrowAmountGetterComponent, PayloadUnescrowAmountGetter<Chain>)]
+pub trait HasPayloadUnescrowAmount<Counterparty, App>:
+    HasPayloadDataType<Counterparty, App>
+where
+    Counterparty: HasAmountType,
+{
+    // Note: the returned unescrow amount is from `Counterparty`, because as incoming packet,
+    // it would be used as `Counterparty::PayloadData` where `Counterparty::Counterparty` = `Self`.
+    fn payload_mint_amount(payload_data: &Self::PayloadData) -> &Counterparty::Amount;
+}
