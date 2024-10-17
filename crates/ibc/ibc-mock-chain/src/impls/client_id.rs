cdc227342eb4d9da37454cde626f13d5df0b838a
Soares Chen <soares.chen@maybevoid.com>
2024-10-17 14:10:52 +0000

    Implement ClientIdFromChannelIdQuerier

---
+++ crates/ibc/ibc-mock-chain/src/impls/client_id.rs
@@ -0,0 +1,30 @@
+use alloc::borrow::ToOwned;
+use alloc::string::String;
+use cgp::core::Async;
+use hermes_ibc_components::traits::queries::client_id::ClientIdFromChannelIdQuerier;
+
+use crate::components::chain::MockChainComponents;
+use crate::contexts::chain::MockChain;
+use crate::types::channel_id::MockChannelId;
+use crate::types::client_id::MockClientId;
+use crate::types::tagged::Tagged;
+
+impl<Chain: Async, Counterparty: Async>
+    ClientIdFromChannelIdQuerier<MockChain<Chain, Counterparty>, MockChain<Counterparty, Chain>>
+    for MockChainComponents
+{
+    async fn query_client_id_from_channel_id(
+        chain: &MockChain<Chain, Counterparty>,
+        channel_id: &Tagged<Chain, Counterparty, MockChannelId>,
+    ) -> Result<Tagged<Chain, Counterparty, MockClientId>, String> {
+        let mut lock = chain.pending_state.lock().await;
+        let state = lock.mock_chain_state_mut();
+
+        let client_id = state
+            .channel_clients
+            .get(channel_id)
+            .ok_or_else(|| "channel not found".to_owned())?;
+
+        Ok(client_id.clone())
+    }
+}
--- crates/ibc/ibc-mock-chain/src/impls/mod.rs
+++ crates/ibc/ibc-mock-chain/src/impls/mod.rs
@@ -1,3 +1,4 @@
+pub mod client_id;
 pub mod commitment;
 pub mod consensus_state;
 pub mod error;
