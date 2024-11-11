806d625d47de249e99ebc0359365d4b8c85512b8
Soares Chen <soares.chen@maybevoid.com>
2024-11-11 19:56:06 +0100

    CLI Components Improvements (#472)
    * Implement WithProvider for type traits
    
    * Implement UseDelegate for ArgParser and CommandRunner
    
    * Make take_chain_process work with &mut ChainDriver
    
    * Pass ConfigUpdater to RunBootstrapChainCommand for non-owned implementation
    
    * Add changelog

--- crates/cli/cli-components/src/traits/parse.rs
+++ crates/cli/cli-components/src/traits/parse.rs
@@ -1,5 +1,6 @@
 use core::marker::PhantomData;
 
+use cgp::core::component::UseDelegate;
 use cgp::prelude::*;
 
 #[derive_component(ArgParserComponent, ArgParser<App>)]
@@ -7,4 +8,21 @@ pub trait CanParseArg<Args, Tag>: HasErrorType {
     type Parsed: Async;
 
     fn parse_arg(&self, args: &Args, tag: PhantomData<Tag>) -> Result<Self::Parsed, Self::Error>;
+}
+
+impl<App, Args, Tag, Components, Delegate> ArgParser<App, Args, Tag> for UseDelegate<Components>
+where
+    App: HasErrorType,
+    Components: DelegateComponent<(Args, Tag), Delegate = Delegate>,
+    Delegate: ArgParser<App, Args, Tag>,
+{
+    type Parsed = Delegate::Parsed;
+
+    fn parse_arg(
+        app: &App,
+        args: &Args,
+        tag: PhantomData<Tag>,
+    ) -> Result<Self::Parsed, App::Error> {
+        Delegate::parse_arg(app, args, tag)
+    }
 }
