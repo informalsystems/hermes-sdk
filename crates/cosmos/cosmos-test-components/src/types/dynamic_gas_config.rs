8b636cc51919eb3d6584f7f87bedc9f31bef91aa
Luca Joss <luca@informal.systems>
2024-11-15 16:49:34 +0100

    Add default implementation for DynamicGasConfig

--- crates/cosmos/cosmos-test-components/src/types/dynamic_gas_config.rs
+++ crates/cosmos/cosmos-test-components/src/types/dynamic_gas_config.rs
@@ -3,3 +3,12 @@ pub struct DynamicGasConfig {
     pub multiplier: f64,
     pub max: f64,
 }
+
+impl Default for DynamicGasConfig {
+    fn default() -> Self {
+        Self {
+            multiplier: 1.1,
+            max: 1.6,
+        }
+    }
+}
