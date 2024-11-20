00d0337a3c7ade700360ce2bf34560ffcec0246e
Luca Joss <luca@informal.systems>
2024-11-20 15:42:47 +0100

    Implement init_bootstrap method to bootstrap CosmosBootstrap using env variables

--- crates/cosmos/cosmos-integration-tests/tests/cosmos_integration_tests.rs
+++ crates/cosmos/cosmos-integration-tests/tests/cosmos_integration_tests.rs
@@ -5,9 +5,7 @@ use std::sync::Arc;
 
 use hermes_cosmos_chain_components::types::config::gas::dynamic_gas_config::DynamicGasConfig;
 use hermes_cosmos_integration_tests::contexts::binary_channel::setup::CosmosBinaryChannelSetup;
-use hermes_cosmos_integration_tests::contexts::bootstrap::CosmosBootstrap;
-use hermes_cosmos_integration_tests::init::init_test_runtime;
-use hermes_cosmos_relayer::contexts::build::CosmosBuilder;
+use hermes_cosmos_integration_tests::init::{init_bootstrap, init_test_runtime};
 use hermes_error::types::Error;
 use hermes_ibc_test_suite::tests::transfer::TestIbcTransfer;
 use hermes_test_components::setup::traits::run_test::CanRunTest;
@@ -19,22 +17,27 @@ use ibc_relayer_types::core::ics24_host::identifier::PortId;
 fn cosmos_integration_tests() -> Result<(), Error> {
     let runtime = init_test_runtime();
 
-    let builder = Arc::new(CosmosBuilder::new_with_default(runtime.clone()));
+    let bootstrap_chain_0 = Arc::new(init_bootstrap(
+        0,
+        runtime.clone(),
+        true,
+        "./test-data",
+        "coin".into(),
+        |_| Ok(()),
+        |_| Ok(()),
+        Some(DynamicGasConfig::default()),
+    ));
 
-    // TODO: load parameters from environment variables
-    let bootstrap = Arc::new(CosmosBootstrap {
-        runtime: runtime.clone(),
-        cosmos_builder: builder,
-        should_randomize_identifiers: true,
-        chain_store_dir: "./test-data".into(),
-        chain_command_path: "gaiad".into(),
-        account_prefix: "cosmos".into(),
-        staking_denom_prefix: "stake".into(),
-        transfer_denom_prefix: "coin".into(),
-        genesis_config_modifier: Box::new(|_| Ok(())),
-        comet_config_modifier: Box::new(|_| Ok(())),
-        dynamic_gas: Some(DynamicGasConfig::default()),
-    });
+    let bootstrap_chain_1 = Arc::new(init_bootstrap(
+        1,
+        runtime.clone(),
+        true,
+        "./test-data",
+        "coin".into(),
+        |_| Ok(()),
+        |_| Ok(()),
+        Some(DynamicGasConfig::default()),
+    ));
 
     let create_client_settings = Settings {
         max_clock_drift: Duration::from_secs(40),
@@ -43,8 +46,8 @@ fn cosmos_integration_tests() -> Result<(), Error> {
     };
 
     let setup = CosmosBinaryChannelSetup {
-        bootstrap_a: bootstrap.clone(),
-        bootstrap_b: bootstrap,
+        bootstrap_a: bootstrap_chain_0,
+        bootstrap_b: bootstrap_chain_1,
         create_client_settings,
         init_connection_options: Default::default(),
         init_channel_options: Default::default(),
