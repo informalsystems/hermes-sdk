7310253aba2193b695196e47ef16be80ad434efb
Soares Chen <soares.chen@maybevoid.com>
2024-11-26 11:39:52 +0000

    Try using new light client

--- crates/cosmos/cosmos-chain-components/src/impls/client/update_client_payload_new.rs
+++ crates/cosmos/cosmos-chain-components/src/impls/client/update_client_payload_new.rs
@@ -1,18 +1,17 @@
 use cgp::prelude::CanRaiseError;
+use hermes_comet_light_client_components::traits::verify_target_height::{
+    CanVerifyTargetHeight, VerifyForward,
+};
+use hermes_comet_light_client_context::contexts::light_client::CometLightClient;
+use hermes_error::types::HermesError;
 use hermes_relayer_components::chain::traits::payload_builders::update_client::UpdateClientPayloadBuilder;
 use hermes_relayer_components::chain::traits::queries::chain_status::CanQueryChainStatus;
 use hermes_relayer_components::chain::traits::types::client_state::HasClientStateType;
-use hermes_relayer_components::chain::traits::types::height::HasHeightType;
+use hermes_relayer_components::chain::traits::types::height::HasHeightFields;
 use hermes_relayer_components::chain::traits::types::update_client::HasUpdateClientPayloadType;
-use ibc::core::client::types::Height;
 use ibc_client_tendermint::types::error::TendermintClientError;
 use ibc_client_tendermint::types::ClientState;
-use tendermint::trust_threshold::TrustThresholdFraction;
-use tendermint_light_client::components::clock::FixedClock;
-use tendermint_light_client::components::io::ProdIo;
-use tendermint_light_client::components::scheduler::basic_bisecting_schedule;
-use tendermint_light_client::light_client::LightClient;
-use tendermint_light_client_verifier::ProdVerifier;
+use tendermint::block::Height as TmHeight;
 use tendermint_rpc::Client;
 
 use crate::traits::rpc_client::HasRpcClient;
@@ -22,19 +21,20 @@ pub struct BuildTendermintUpdateClientPayload;
 impl<Chain, Counterparty> UpdateClientPayloadBuilder<Chain, Counterparty>
     for BuildTendermintUpdateClientPayload
 where
-    Chain: HasHeightType<Height = Height>
+    Chain: HasHeightFields
         + CanQueryChainStatus
         + HasRpcClient
         + HasUpdateClientPayloadType<Counterparty>
         + HasClientStateType<Counterparty, ClientState = ClientState>
         + CanRaiseError<tendermint::Error>
         + CanRaiseError<tendermint_rpc::Error>
-        + CanRaiseError<TendermintClientError>,
+        + CanRaiseError<TendermintClientError>
+        + CanRaiseError<HermesError>,
 {
     async fn build_update_client_payload(
         chain: &Chain,
         _trusted_height: &Chain::Height,
-        _target_height: &Chain::Height,
+        target_height: &Chain::Height,
         client_state: Chain::ClientState,
     ) -> Result<Chain::UpdateClientPayload, Chain::Error> {
         let rpc_client = chain.rpc_client();
@@ -43,35 +43,24 @@ where
         let current_time = status.sync_info.latest_block_time;
         let peer_id = status.node_info.id;
 
-        let clock = FixedClock::new(current_time);
-
-        let verifier = ProdVerifier::default();
-
-        let _trust_threshold = TrustThresholdFraction::new(
-            client_state.trust_level.numerator(),
-            client_state.trust_level.denominator(),
-        )
-        .map_err(Chain::raise_error)?;
-
         let light_client_options = client_state
             .as_light_client_options()
             .map_err(Chain::raise_error)?;
 
-        let io = ProdIo::new(peer_id, rpc_client.clone(), None);
-
-        // let trusted_block_height = trusted_height.revision_height().try_into()
-        //     .map_err(Chain::raise_error)?;
-
-        // let block = io.fetch_light_block(AtHeight::At(trusted_block_height));
-
-        let _light_client = LightClient::new(
+        let mut light_client = CometLightClient::new(
+            current_time,
             peer_id,
+            rpc_client.clone(),
             light_client_options,
-            clock,
-            basic_bisecting_schedule,
-            verifier,
-            io,
         );
+
+        let target_tm_height = TmHeight::try_from(Chain::revision_height(target_height))
+            .map_err(Chain::raise_error)?;
+
+        light_client
+            .verify_target_height(VerifyForward, &target_tm_height)
+            .await
+            .map_err(Chain::raise_error)?;
 
         todo!()
     }
