80a965e9a816328c21633bd74b6927100db8152a
Soares Chen <soares.chen@maybevoid.com>
2024-11-26 19:22:29 +0000

    Use back Hermes v1 types for now

--- crates/cosmos/cosmos-chain-components/src/impls/client/update_client_payload_new.rs
+++ crates/cosmos/cosmos-chain-components/src/impls/client/update_client_payload_new.rs
@@ -7,14 +7,15 @@ use hermes_relayer_components::chain::traits::payload_builders::update_client::U
 use hermes_relayer_components::chain::traits::queries::chain_status::CanQueryChainStatus;
 use hermes_relayer_components::chain::traits::types::client_state::HasClientStateType;
 use hermes_relayer_components::chain::traits::types::update_client::HasUpdateClientPayloadType;
-use ibc::core::client::types::error::ClientError;
-use ibc::core::client::types::Height;
-use ibc_client_tendermint::types::error::TendermintClientError;
-use ibc_client_tendermint::types::{ClientState, Header};
+use ibc_relayer_types::clients::ics07_tendermint::header::Header;
+use ibc_relayer_types::core::ics02_client::error::Error as ClientError;
+use ibc_relayer_types::Height;
 use tendermint::block::Height as TmHeight;
 use tendermint_rpc::Client;
 
 use crate::traits::rpc_client::HasRpcClient;
+use crate::types::payloads::client::CosmosUpdateClientPayload;
+use crate::types::tendermint::TendermintClientState;
 
 pub struct BuildTendermintUpdateClientPayload;
 
@@ -24,11 +25,10 @@ where
     Chain: HasHeightType<Height = Height>
         + CanQueryChainStatus
         + HasRpcClient
-        + HasUpdateClientPayloadType<Counterparty>
-        + HasClientStateType<Counterparty, ClientState = ClientState>
+        + HasUpdateClientPayloadType<Counterparty, UpdateClientPayload = CosmosUpdateClientPayload>
+        + HasClientStateType<Counterparty, ClientState = TendermintClientState>
         + CanRaiseError<tendermint::Error>
         + CanRaiseError<tendermint_rpc::Error>
-        + CanRaiseError<TendermintClientError>
         + CanRaiseError<ClientError>
         + CanRaiseError<HermesError>,
 {
@@ -36,7 +36,7 @@ where
         chain: &Chain,
         trusted_height: &Height,
         target_height: &Height,
-        client_state: ClientState,
+        client_state: TendermintClientState,
     ) -> Result<Chain::UpdateClientPayload, Chain::Error> {
         let rpc_client = chain.rpc_client();
         let status = rpc_client.status().await.map_err(Chain::raise_error)?;
@@ -44,9 +44,7 @@ where
         let current_time = status.sync_info.latest_block_time;
         let peer_id = status.node_info.id;
 
-        let light_client_options = client_state
-            .as_light_client_options()
-            .map_err(Chain::raise_error)?;
+        let light_client_options = client_state.as_light_client_options();
 
         let mut light_client = CometLightClient::new(
             current_time,
@@ -71,20 +69,21 @@ where
 
         let revision_number = target_height.revision_number();
 
-        let _headers = target_blocks
+        let headers = target_blocks
             .zip(blocks.iter())
             .map(|(target_block, trusted_block)| {
                 let trusted_height = Height::new(revision_number, trusted_block.height().into())
                     .map_err(Chain::raise_error)?;
 
-                <Result<_, Chain::Error>>::Ok(Header {
+                Ok(Header {
                     signed_header: target_block.signed_header.clone(),
                     validator_set: target_block.validators.clone(),
                     trusted_height,
-                    trusted_next_validator_set: trusted_block.validators.clone(),
+                    trusted_validator_set: trusted_block.validators.clone(),
                 })
-            });
+            })
+            .collect::<Result<Vec<_>, Chain::Error>>()?;
 
-        todo!()
+        Ok(CosmosUpdateClientPayload { headers })
     }
 }
