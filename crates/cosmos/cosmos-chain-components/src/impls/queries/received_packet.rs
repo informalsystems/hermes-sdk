ec192d7f293e1effa51a77094f3a90a8cf398569
Luca Joss <luca@informal.systems>
2024-12-02 10:09:52 +0100

    Improve query_packet_is_received implementation for Cosmos Chain

--- crates/cosmos/cosmos-chain-components/src/impls/queries/received_packet.rs
+++ crates/cosmos/cosmos-chain-components/src/impls/queries/received_packet.rs
@@ -3,13 +3,11 @@ use tonic::transport::Error as TransportError;
 use tonic::Status;
 
 use hermes_relayer_components::chain::traits::queries::packet_is_received::ReceivedPacketQuerier;
+use hermes_relayer_components::chain::traits::queries::unreceived_packet_sequences::CanQueryUnreceivedPacketSequences;
 use hermes_relayer_components::chain::traits::types::ibc::HasIbcChainTypes;
 
-use ibc_relayer::chain::requests::QueryUnreceivedPacketsRequest;
 use ibc_relayer_types::core::ics04_channel::packet::Sequence;
 use ibc_relayer_types::core::ics24_host::identifier::{ChannelId, PortId};
-
-use ibc_proto::ibc::core::channel::v1::query_client::QueryClient;
 
 use crate::traits::grpc_address::HasGrpcAddress;
 
@@ -19,6 +17,7 @@ impl<Chain, Counterparty> ReceivedPacketQuerier<Chain, Counterparty> for QueryCo
 where
     Chain: HasIbcChainTypes<Counterparty, ChannelId = ChannelId, PortId = PortId>
         + HasGrpcAddress
+        + CanQueryUnreceivedPacketSequences<Counterparty>
         + CanRaiseError<TransportError>
         + CanRaiseError<Status>,
     Counterparty: HasIbcChainTypes<Chain, Sequence = Sequence>,
@@ -29,28 +28,12 @@ where
         channel_id: &Chain::ChannelId,
         sequence: &Counterparty::Sequence,
     ) -> Result<bool, Chain::Error> {
-        let port_id = port_id.clone();
-        let channel_id = channel_id.clone();
         let sequence = *sequence;
 
-        let mut client = QueryClient::connect(chain.grpc_address().clone())
-            .await
-            .map_err(Chain::raise_error)?;
+        let unreceived_sequences = chain
+            .query_unreceived_packet_sequences(channel_id, port_id, &[sequence])
+            .await?;
 
-        let raw_req = QueryUnreceivedPacketsRequest {
-            port_id: port_id.clone(),
-            channel_id: channel_id.clone(),
-            packet_commitment_sequences: vec![sequence],
-        };
-
-        let req = tonic::Request::new(raw_req.into());
-
-        let response = client
-            .unreceived_packets(req)
-            .await
-            .map_err(Chain::raise_error)?
-            .into_inner();
-
-        Ok(response.sequences.is_empty())
+        Ok(unreceived_sequences.is_empty())
     }
 }
