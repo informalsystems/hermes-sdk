985ea3af0b9066a56af333830c6ae5d592201951
Soares Chen <soares.chen@maybevoid.com>
2024-12-04 09:06:02 +0000

    Drafting new RelayTarget trait

--- crates/relayer/relayer-components/src/relay/traits/target.rs
+++ crates/relayer/relayer-components/src/relay/traits/target.rs
@@ -3,6 +3,8 @@ use cgp::core::Async;
 
 use crate::chain::traits::types::ibc::HasIbcChainTypes;
 use crate::chain::types::aliases::ClientIdOf;
+use crate::multi::traits::chain_at::HasChainTypeAt;
+use crate::multi::types::tags::{Dst, Src};
 use crate::relay::traits::chains::{CanRaiseRelayChainErrors, HasRelayChains, HasRelayClientIds};
 
 #[derive(Default, Clone, Copy)]
@@ -10,6 +12,48 @@ pub struct SourceTarget;
 
 #[derive(Default, Clone, Copy)]
 pub struct DestinationTarget;
+
+pub trait RelayTarget: Async + Default + Copy + private::Sealed {
+    type Chain: Async;
+
+    type Counterparty: Async;
+}
+
+impl RelayTarget for SourceTarget {
+    type Chain = Src;
+
+    type Counterparty = Dst;
+}
+
+impl RelayTarget for DestinationTarget {
+    type Chain = Dst;
+
+    type Counterparty = Src;
+}
+
+pub trait HasTargetChainTypes<Target: RelayTarget>:
+    HasErrorType
+    + HasChainTypeAt<Target::Chain, Chain = Self::TargetChain>
+    + HasChainTypeAt<Target::Counterparty, Chain = Self::CounterpartyChain>
+{
+    type TargetChain: HasIbcChainTypes<Self::CounterpartyChain> + HasErrorType;
+
+    type CounterpartyChain: HasIbcChainTypes<Self::TargetChain> + HasErrorType;
+}
+
+impl<Relay, Target, TargetChain, CounterpartyChain> HasTargetChainTypes<Target> for Relay
+where
+    Target: RelayTarget,
+    Relay: HasErrorType
+        + HasChainTypeAt<Target::Chain, Chain = TargetChain>
+        + HasChainTypeAt<Target::Counterparty, Chain = CounterpartyChain>,
+    TargetChain: HasIbcChainTypes<CounterpartyChain> + HasErrorType,
+    CounterpartyChain: HasIbcChainTypes<TargetChain> + HasErrorType,
+{
+    type TargetChain = TargetChain;
+
+    type CounterpartyChain = CounterpartyChain;
+}
 
 pub trait ChainTargetType<Relay>: Async + Default + Copy + private::Sealed {
     type TargetChain: HasIbcChainTypes<Self::CounterpartyChain> + HasErrorType;
