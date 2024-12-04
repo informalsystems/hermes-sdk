daf2a3527504e0b2d946d4d11f16965da8bdcd24
Luca Joss <luca@informal.systems>
2024-12-04 14:22:55 +0100

    Remove legacy bootstrap test and unnecessary code

--- crates/cosmos/cosmos-integration-tests/tests/cosmos_integration_tests.rs
+++ crates/cosmos/cosmos-integration-tests/tests/cosmos_integration_tests.rs
@@ -12,16 +12,6 @@ use hermes_test_components::test_case::traits::test_case::TestCase;
 fn cosmos_integration_tests() -> Result<(), Error> {
     let runtime = init_test_runtime();
 
-    /*let setup = CosmosBinaryChannelSetup {
-        bootstrap_a: bootstrap_chain_0,
-        bootstrap_b: bootstrap_chain_1,
-        builder,
-        create_client_payload_options: Default::default(),
-        init_connection_options: Default::default(),
-        init_channel_options: Default::default(),
-        port_id: PortId::transfer(),
-    };*/
-
     // TODO: Use a test suite entry point for running multiple tests
     runtime.runtime.clone().block_on(async move {
         let setup: CosmosBinaryChannelTestDriver = init_preset_bootstraps::<
