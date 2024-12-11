4936ff00df71f92164f4e7254bc4615e2e0b88e1
Soares Chen <soares.chen@maybevoid.com>
2024-12-11 13:04:12 +0000

    Remove relay impl of FilterPacketWithConfig

--- crates/cosmos/cosmos-chain-components/src/impls/relay/packet_filter.rs
+++ crates/cosmos/cosmos-chain-components/src/impls/relay/packet_filter.rs
@@ -7,33 +7,11 @@ use hermes_relayer_components::chain::traits::packet::filter::{
     IncomingPacketFilter, OutgoingPacketFilter,
 };
 use hermes_relayer_components::chain::traits::types::ibc::{HasChannelIdType, HasPortIdType};
-use hermes_relayer_components::relay::traits::chains::{HasRelayChainTypes, HasRelayPacketType};
-use hermes_relayer_components::relay::traits::packet_filter::RelayPacketFilter;
 use ibc::core::host::types::identifiers::{ChannelId, PortId};
 
 use crate::types::messages::packet::packet_filter::PacketFilterConfig;
 
 pub struct FilterPacketWithConfig<Tag>(pub PhantomData<Tag>);
-
-impl<Relay, Tag, SrcChain, DstChain> RelayPacketFilter<Relay> for FilterPacketWithConfig<Tag>
-where
-    Relay: HasRelayChainTypes<SrcChain = SrcChain, DstChain = DstChain>
-        + HasRelayPacketType
-        + HasField<Tag, Value = PacketFilterConfig>,
-    SrcChain: CanReadOutgoingPacketFields<DstChain>
-        + HasPortIdType<DstChain, PortId = PortId>
-        + HasChannelIdType<DstChain, ChannelId = ChannelId>,
-{
-    async fn should_relay_packet(
-        relay: &Relay,
-        packet: &Relay::Packet,
-    ) -> Result<bool, Relay::Error> {
-        Ok(relay.get_field(PhantomData).is_allowed(
-            SrcChain::outgoing_packet_src_port(packet),
-            SrcChain::outgoing_packet_src_channel_id(packet),
-        ))
-    }
-}
 
 impl<Chain, Counterparty, Tag> OutgoingPacketFilter<Chain, Counterparty>
     for FilterPacketWithConfig<Tag>
