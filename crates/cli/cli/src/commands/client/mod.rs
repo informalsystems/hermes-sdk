1b0b9c903950ff49edeaf2e36dcac50f78ff81df
Luca Joss <luca@informal.systems>
2024-12-24 12:00:51 +0100

    Remove cosmos-specific implementation of client update CLI

--- crates/cli/cli/src/commands/client/mod.rs
+++ crates/cli/cli/src/commands/client/mod.rs
@@ -1,3 +1,4 @@
+use hermes_cli_components::impls::commands::client::update::UpdateClientArgs;
 use hermes_cli_components::traits::command::CanRunCommand;
 use hermes_cli_framework::command::CommandRunner;
 use hermes_cli_framework::output::Output;
@@ -8,23 +9,20 @@ use crate::Result;
 
 pub mod create;
 
-mod update;
-pub use update::ClientUpdate;
-
 #[derive(Debug, clap::Subcommand)]
 pub enum ClientCommands {
     /// Create a new client
     Create(CreateClientArgs),
 
     /// Update a client
-    Update(ClientUpdate),
+    Update(UpdateClientArgs),
 }
 
 impl CommandRunner<HermesApp> for ClientCommands {
     async fn run(&self, app: &HermesApp) -> Result<Output> {
         match self {
             Self::Create(cmd) => app.run_command(cmd).await,
-            Self::Update(cmd) => cmd.run(app).await,
+            Self::Update(cmd) => app.run_command(cmd).await,
         }
     }
 }
