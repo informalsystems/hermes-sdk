c163c4b303eb2866bc6628788853593af060173b
Sean Chen <seanchen11235@gmail.com>
2025-01-16 11:24:37 -0600

    Wire up create channel cli

--- crates/cli/cli/src/commands/channel/mod.rs
+++ crates/cli/cli/src/commands/channel/mod.rs
@@ -1,5 +1,5 @@
-pub mod create;
-pub use create::ChannelCreate;
+use hermes_cli_components::impls::commands::channel::create::CreateChannelArgs;
+use hermes_cli_components::traits::command::CanRunCommand;
 use hermes_cli_framework::command::CommandRunner;
 use hermes_cli_framework::output::Output;
 
@@ -9,13 +9,13 @@ use crate::Result;
 #[derive(Debug, clap::Subcommand)]
 pub enum ChannelCommands {
     /// Create a new channel
-    Create(ChannelCreate),
+    Create(CreateChannelArgs),
 }
 
 impl CommandRunner<HermesApp> for ChannelCommands {
     async fn run(&self, app: &HermesApp) -> Result<Output> {
         match self {
-            Self::Create(cmd) => cmd.run(app).await,
+            Self::Create(cmd) => app.run_command(cmd).await,
         }
     }
 }
