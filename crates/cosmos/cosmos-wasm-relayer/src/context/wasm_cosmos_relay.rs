f3aa76c880cabff87f4a00889758a7ce3a800d15
Soares Chen <soares.chen@maybevoid.com>
2025-02-10 13:00:18 +0000

    Remove unused imports

--- crates/cosmos/cosmos-wasm-relayer/src/context/wasm_cosmos_relay.rs
+++ crates/cosmos/cosmos-wasm-relayer/src/context/wasm_cosmos_relay.rs
@@ -17,9 +17,7 @@ use hermes_logging_components::traits::logger::CanLog;
 use hermes_relayer_components::chain::traits::types::channel::HasInitChannelOptionsType;
 use hermes_relayer_components::chain::traits::types::connection::HasInitConnectionOptionsType;
 use hermes_relayer_components::components::default::relay::*;
-use hermes_relayer_components::error::traits::retry::{
-    MaxErrorRetryGetterComponent, RetryableErrorComponent,
-};
+use hermes_relayer_components::error::traits::retry::RetryableErrorComponent;
 use hermes_relayer_components::multi::traits::chain_at::{
     ChainGetterAtComponent, ChainTypeAtComponent,
 };
@@ -29,10 +27,7 @@ use hermes_relayer_components::relay::impls::channel::bootstrap::CanBootstrapCha
 use hermes_relayer_components::relay::impls::connection::bootstrap::CanBootstrapConnection;
 use hermes_relayer_components::relay::impls::packet_lock::PacketMutexGetterComponent;
 use hermes_relayer_components::relay::impls::packet_relayers::general::lock::LogSkipRelayLockedPacket;
-use hermes_relayer_components::relay::traits::packet_filter::RelayPacketFilterComponent;
-use hermes_relayer_components::relay::traits::packet_lock::PacketLockComponent;
 use hermes_relayer_components::relay::traits::packet_relayer::CanRelayPacket;
-use hermes_relayer_components::with_default_relay_preset;
 use hermes_runtime::types::runtime::HermesRuntime;
 use hermes_runtime_components::traits::runtime::{RuntimeGetterComponent, RuntimeTypeComponent};
 use ibc::core::host::types::identifiers::{ChannelId, ClientId, PortId, Sequence};
--- crates/relayer/relayer-components-extra/src/components/extra/build.rs
+++ crates/relayer/relayer-components-extra/src/components/extra/build.rs
@@ -1,35 +1,15 @@
-use cgp::core::error::ProvideErrorType;
-use cgp::core::field::Index;
 use cgp::prelude::*;
 pub use hermes_relayer_components::build::traits::builders::birelay_builder::{
     BiRelayBuilderComponent, CanBuildBiRelay,
 };
-use hermes_relayer_components::build::traits::builders::birelay_from_relay_builder::BiRelayFromRelayBuilder;
 pub use hermes_relayer_components::build::traits::builders::chain_builder::{
     ChainBuilder, ChainBuilderComponent,
 };
 pub use hermes_relayer_components::build::traits::builders::relay_builder::RelayBuilderComponent;
-use hermes_relayer_components::build::traits::builders::relay_from_chains_builder::CanBuildRelayFromChains;
 pub use hermes_relayer_components::build::traits::builders::relay_from_chains_builder::RelayFromChainsBuilderComponent;
-use hermes_relayer_components::build::traits::cache::{HasChainCache, HasRelayCache};
-use hermes_relayer_components::chain::traits::types::chain_id::HasChainId;
-use hermes_relayer_components::chain::traits::types::ibc::HasIbcChainTypes;
 use hermes_relayer_components::components::default::build::DefaultBuildComponents;
-use hermes_relayer_components::multi::traits::birelay_at::HasBiRelayTypeAt;
-use hermes_relayer_components::multi::traits::chain_at::HasChainTypeAt;
-use hermes_relayer_components::multi::traits::relay_at::HasRelayTypeAt;
-use hermes_relayer_components::relay::traits::chains::{
-    CanRaiseRelayChainErrors, HasRelayChains, HasRelayClientIds,
-};
-use hermes_relayer_components::relay::traits::target::{DestinationTarget, SourceTarget};
-use hermes_runtime_components::traits::channel::{CanCloneSender, CanCreateChannels};
-use hermes_runtime_components::traits::channel_once::CanUseChannelsOnce;
-use hermes_runtime_components::traits::runtime::{HasRuntime, HasRuntimeType};
 
-use crate::batch::traits::config::HasBatchConfig;
 use crate::build::impls::relay::batch::BuildRelayWithBatchWorker;
-use crate::build::traits::cache::HasBatchSenderCache;
-use crate::build::traits::relay_with_batch_builder::RelayWithBatchBuilder;
 
 cgp_preset! {
     ExtraBuildComponents<BaseComponents: Async> {
