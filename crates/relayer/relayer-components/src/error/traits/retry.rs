fc17e8fb964adfe6f37f87b71007c7d8037d1a05
Soares Chen <soares.chen@maybevoid.com>
2025-02-28 08:00:54 +0000

    Define CanPerformRetry trait

--- crates/relayer/relayer-components/src/error/traits/retry.rs
+++ crates/relayer/relayer-components/src/error/traits/retry.rs
@@ -1,8 +1,19 @@
 use cgp::prelude::*;
 
 #[cgp_component {
-  name: RetryableErrorComponent,
-  provider: ProvideRetryableError,
+    provider: RetryPerformer,
+}]
+#[async_trait]
+pub trait CanPerformRetry: HasAsyncErrorType {
+    async fn perform_retry<T: Async>(
+        num_retries: usize,
+        cont: impl AsyncFn() -> Result<T, Self::Error> + Send,
+    ) -> Result<T, Self::Error>;
+}
+
+#[cgp_component {
+    name: RetryableErrorComponent,
+    provider: ProvideRetryableError,
 }]
 pub trait HasRetryableError: HasAsyncErrorType {
     fn is_retryable_error(e: &Self::Error) -> bool;
