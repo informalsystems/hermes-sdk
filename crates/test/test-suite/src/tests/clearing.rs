0f53dd23806c95caa58b8652d5e3490bfd13e3af
Luca Joss <luca@informal.systems>
2025-03-03 14:41:51 +0100

    Fix packet clearing test to test both recv and ack clearing

--- crates/test/test-suite/src/tests/clearing.rs
+++ crates/test/test-suite/src/tests/clearing.rs
@@ -7,8 +7,12 @@ use cgp::prelude::*;
 use hermes_logging_components::traits::has_logger::HasLogger;
 use hermes_logging_components::traits::logger::CanLogMessage;
 use hermes_relayer_components::birelay::traits::HasTwoWayRelay;
-use hermes_relayer_components::chain::traits::packet::fields::CanReadPacketFields;
-use hermes_relayer_components::chain::traits::queries::chain_status::CanQueryChainHeight;
+use hermes_relayer_components::chain::traits::packet::fields::{
+    CanReadPacketFields, HasPacketSequence,
+};
+use hermes_relayer_components::chain::traits::queries::chain_status::{
+    CanQueryChainHeight, CanQueryChainStatus,
+};
 use hermes_relayer_components::chain::traits::queries::packet_is_cleared::CanQueryPacketIsCleared;
 use hermes_relayer_components::chain::traits::queries::packet_is_received::CanQueryPacketIsReceived;
 use hermes_relayer_components::chain::traits::types::chain_id::HasChainId;
@@ -19,12 +23,14 @@ use hermes_relayer_components::multi::traits::chain_at::HasChainTypeAt;
 use hermes_relayer_components::multi::traits::relay_at::RelayAt;
 use hermes_relayer_components::relay::traits::auto_relayer::CanAutoRelayWithHeights;
 use hermes_relayer_components::relay::traits::chains::HasRelayChainTypes;
+use hermes_relayer_components::relay::traits::packet_relayers::receive_packet::CanRelayReceivePacket;
 use hermes_relayer_components::relay::traits::target::{HasSourceTargetChainTypes, SourceTarget};
 use hermes_test_components::chain::traits::queries::balance::CanQueryBalance;
 use hermes_test_components::chain::traits::transfer::amount::CanConvertIbcTransferredAmount;
 use hermes_test_components::chain::traits::transfer::ibc_transfer::CanIbcTransferToken;
 use hermes_test_components::chain::traits::types::amount::HasAmountMethods;
 use hermes_test_components::chain::traits::types::memo::HasDefaultMemo;
+use hermes_test_components::chain::traits::types::wallet::HasWalletType;
 use hermes_test_components::chain_driver::traits::fields::amount::CanGenerateRandomAmount;
 use hermes_test_components::chain_driver::traits::fields::denom_at::{HasDenomAt, TransferDenom};
 use hermes_test_components::chain_driver::traits::fields::wallet::{HasWalletAt, UserWallet};
@@ -53,8 +59,10 @@ where
         + HasWalletAt<UserWallet, Index<0>>
         + HasWalletAt<UserWallet, Index<1>>
         + CanGenerateRandomAmount,
-    ChainDriverB:
-        HasChain<Chain = ChainB> + HasWalletAt<UserWallet, Index<0>> + CanGenerateRandomAmount,
+    ChainDriverB: HasChain<Chain = ChainB>
+        + HasWalletAt<UserWallet, Index<0>>
+        + HasDenomAt<TransferDenom, Index<0>>
+        + CanGenerateRandomAmount,
     RelayDriver: HasBiRelayAt<Index<0>, Index<1>, BiRelay = BiRelay>,
     ChainA: HasIbcChainTypes<ChainB>
         + HasChainId
@@ -67,7 +75,8 @@ where
         + CanQueryPacketIsReceived<ChainB>
         + CanQueryPacketIsCleared<ChainB>
         + CanReadPacketFields<ChainB>
-        + HasDefaultMemo,
+        + HasDefaultMemo
+        + CanQueryChainStatus,
     ChainB: HasIbcChainTypes<ChainA>
         + HasChainId
         + HasOutgoingPacketType<ChainA>
@@ -79,13 +88,16 @@ where
         + CanQueryPacketIsReceived<ChainA>
         + CanQueryPacketIsCleared<ChainA>
         + CanReadPacketFields<ChainA>
-        + HasDefaultMemo,
+        + HasDefaultMemo
+        + CanQueryChainStatus,
     BiRelay: HasTwoWayRelay,
     RelayAt<BiRelay, Index<0>, Index<1>>: HasRelayChainTypes<SrcChain = ChainA, DstChain = ChainB>
         + HasSourceTargetChainTypes
+        + CanRelayReceivePacket
         + CanAutoRelayWithHeights<SourceTarget>,
     RelayAt<BiRelay, Index<1>, Index<0>>: HasRelayChainTypes<SrcChain = ChainB, DstChain = ChainA>
         + HasSourceTargetChainTypes
+        + CanRelayReceivePacket
         + CanAutoRelayWithHeights<SourceTarget>,
     Logger: CanLogMessage,
     Driver::Error: From<ChainA::Error>
@@ -116,20 +128,6 @@ where
 
         let chain_id_b = chain_b.chain_id();
 
-        let wallet_a1 = chain_driver_a.wallet_at(UserWallet, PhantomData::<Index<0>>);
-
-        let address_a1 = ChainA::wallet_address(wallet_a1);
-
-        let wallet_b = chain_driver_b.wallet_at(UserWallet, PhantomData::<Index<0>>);
-
-        let address_b = ChainB::wallet_address(wallet_b);
-
-        let denom_a = chain_driver_a.denom_at(TransferDenom, PhantomData::<Index<0>>);
-
-        let balance_a1 = chain_a.query_balance(address_a1, denom_a).await?;
-
-        let a_to_b_amount = chain_driver_a.random_amount(1000, &balance_a1).await;
-
         let channel_id_a = driver.channel_id_at(PhantomData::<(Index<0>, Index<1>)>);
 
         let port_id_a = driver.port_id_at(PhantomData::<(Index<0>, Index<1>)>);
@@ -138,170 +136,234 @@ where
 
         let port_id_b = driver.port_id_at(PhantomData::<(Index<1>, Index<0>)>);
 
-        let height_a1 = chain_a.query_chain_height().await?;
-
         logger
             .log_message(&format!(
-                "Sending IBC transfer from chain {} to chain {} with amount of {} {}",
-                chain_id_a, chain_id_b, a_to_b_amount, denom_a
+                "Test clearing pending recv from chain `{chain_id_a}` to chain `{chain_id_b}` and pending ack from `{chain_id_b}` to `{chain_id_a}`"
             ))
             .await;
 
-        let packet_a = chain_a
-            .ibc_transfer_token(
-                channel_id_a,
-                port_id_a,
-                wallet_a1,
-                address_b,
-                &a_to_b_amount,
-                &chain_a.default_memo(),
-            )
-            .await?;
-
-        let sequence_a = ChainA::packet_sequence(&packet_a);
-
-        let balance_a2 = ChainA::subtract_amount(&balance_a1, &a_to_b_amount)?;
-
-        let balance_a3 = chain_a.query_balance(address_a1, denom_a).await?;
-
-        assert_eq!(balance_a2, balance_a3);
-
-        let height_a2 = chain_a.query_chain_height().await?;
-
-        {
-            let is_received = chain_b
-                .query_packet_is_received(port_id_b, channel_id_b, &sequence_a)
-                .await?;
-
-            assert!(!is_received);
-
-            let is_cleared = chain_a
-                .query_packet_is_cleared(port_id_a, channel_id_a, &sequence_a)
-                .await?;
-
-            assert!(!is_cleared);
-        }
-
-        relay_a_to_b
-            .auto_relay_with_heights(SourceTarget, &height_a1, Some(&height_a2))
-            .await?;
-
-        let balance_b1 = ChainB::ibc_transfer_amount_from(&a_to_b_amount, channel_id_b, port_id_b)?;
-
-        {
-            let balance_b2 = chain_b
-                .query_balance(address_b, ChainB::amount_denom(&balance_b1))
-                .await?;
-
-            assert_eq!(balance_b2, balance_b1);
-        }
-
-        {
-            let is_received = chain_b
-                .query_packet_is_received(port_id_b, channel_id_b, &sequence_a)
-                .await?;
-
-            assert!(is_received);
-
-            let is_cleared = chain_a
-                .query_packet_is_cleared(port_id_a, channel_id_a, &sequence_a)
-                .await?;
-
-            assert!(is_cleared);
-        }
-
-        let height_b1 = chain_b.query_chain_height().await?;
-
-        let wallet_a2 = chain_driver_a.wallet_at(UserWallet, PhantomData::<Index<1>>);
-
-        let address_a2 = ChainA::wallet_address(wallet_a2);
-
-        let b_to_a_amount = chain_driver_b.random_amount(500, &balance_b1).await;
+        run_one_way_clearing_test::<
+            Driver,
+            RelayAt<BiRelay, Index<0>, Index<1>>,
+            ChainA,
+            ChainDriverA,
+            ChainB,
+            ChainDriverB,
+        >(
+            chain_driver_a,
+            chain_driver_b,
+            relay_a_to_b,
+            channel_id_a,
+            port_id_a,
+            channel_id_b,
+            port_id_b,
+        )
+        .await?;
 
         logger
             .log_message(&format!(
-                "Sending IBC transfer from chain {} to chain {} with amount of {}",
-                chain_id_b, chain_id_a, b_to_a_amount,
+                "Test clearing pending recv from chain `{chain_id_b}` to chain `{chain_id_a}` and pending ack from `{chain_id_a}` to `{chain_id_b}`"
             ))
             .await;
 
-        let packet_b = chain_b
-            .ibc_transfer_token(
-                channel_id_b,
-                port_id_b,
-                wallet_b,
-                address_a2,
-                &b_to_a_amount,
-                &chain_b.default_memo(),
-            )
-            .await?;
-
-        let sequence_b = ChainB::packet_sequence(&packet_b);
-
-        let balance_b2 = ChainB::subtract_amount(&balance_b1, &b_to_a_amount)?;
-
-        let denom_b = ChainB::amount_denom(&balance_b1);
-
-        let balance_b3 = chain_b.query_balance(address_b, denom_b).await?;
-
-        assert_eq!(balance_b2, balance_b3);
-
-        let balance_a4 = chain_a.query_balance(address_a2, denom_a).await?;
-
-        let balance_a5 = ChainA::add_amount(
-            &balance_a4,
-            &ChainA::transmute_counterparty_amount(&b_to_a_amount, denom_a),
-        )?;
-
-        let height_b2 = chain_b.query_chain_height().await?;
-
-        {
-            let is_received = chain_a
-                .query_packet_is_received(port_id_a, channel_id_a, &sequence_b)
-                .await?;
-
-            assert!(!is_received);
-
-            let is_cleared = chain_b
-                .query_packet_is_cleared(port_id_b, channel_id_b, &sequence_b)
-                .await?;
-
-            assert!(!is_cleared);
-        }
-
-        relay_b_to_a
-            .auto_relay_with_heights(SourceTarget, &height_b1, Some(&height_b2))
-            .await?;
-
-        {
-            let balance_a6 = chain_a
-                .query_balance(address_a2, ChainA::amount_denom(&balance_a5))
-                .await?;
-
-            assert_eq!(balance_a6, balance_a5);
-        }
-
-        {
-            let is_received = chain_a
-                .query_packet_is_received(port_id_a, channel_id_a, &sequence_b)
-                .await?;
-
-            assert!(is_received);
-
-            let is_cleared = chain_b
-                .query_packet_is_cleared(port_id_b, channel_id_b, &sequence_b)
-                .await?;
-
-            assert!(is_cleared);
-        }
+        run_one_way_clearing_test::<
+            Driver,
+            RelayAt<BiRelay, Index<1>, Index<0>>,
+            ChainB,
+            ChainDriverB,
+            ChainA,
+            ChainDriverA,
+        >(
+            chain_driver_b,
+            chain_driver_a,
+            relay_b_to_a,
+            channel_id_b,
+            port_id_b,
+            channel_id_a,
+            port_id_a,
+        )
+        .await?;
 
         logger
             .log_message(&format!(
-                "successfully performed reverse IBC transfer from chain {} back to chain {}",
-                chain_id_b, chain_id_a,
+                "successfully performed packet clearing between chain `{chain_id_a}` and chain `{chain_id_b}`"
             ))
             .await;
 
         Ok(())
     }
+}
+
+async fn run_one_way_clearing_test<
+    Driver,
+    Relay,
+    SrcChain,
+    SrcChainDriver,
+    DstChain,
+    DstChainDriver,
+>(
+    src_chain_driver: &SrcChainDriver,
+    dst_chain_driver: &DstChainDriver,
+    relay: &Relay,
+    src_channel_id: &SrcChain::ChannelId,
+    src_port_id: &SrcChain::PortId,
+    dst_channel_id: &DstChain::ChannelId,
+    dst_port_id: &DstChain::PortId,
+) -> Result<(), Driver::Error>
+where
+    Driver: HasAsyncErrorType,
+    Relay: HasRelayChainTypes<SrcChain = SrcChain, DstChain = DstChain>
+        + HasSourceTargetChainTypes
+        + CanRelayReceivePacket
+        + CanAutoRelayWithHeights<SourceTarget>,
+    SrcChainDriver: HasChain<Chain = SrcChain>
+        + HasDenomAt<TransferDenom, Index<0>>
+        + HasWalletAt<UserWallet, Index<0>>
+        + CanGenerateRandomAmount,
+    DstChainDriver: HasChain<Chain = DstChain> + HasWalletAt<UserWallet, Index<0>>,
+    SrcChain: CanQueryBalance
+        + HasDefaultMemo
+        + CanIbcTransferToken<DstChain>
+        + CanQueryPacketIsCleared<DstChain>
+        + CanQueryChainStatus
+        + HasPacketSequence<DstChain>
+        + HasAmountMethods,
+    DstChain: HasWalletType + CanQueryPacketIsReceived<SrcChain>,
+    Driver::Error: From<SrcChain::Error> + From<DstChain::Error> + From<Relay::Error>,
+{
+    let sender_wallet = src_chain_driver.wallet_at(UserWallet, PhantomData::<Index<0>>);
+
+    let sender_address = SrcChain::wallet_address(sender_wallet);
+
+    let receiver_wallet = dst_chain_driver.wallet_at(UserWallet, PhantomData::<Index<0>>);
+
+    let receiver_address = DstChain::wallet_address(receiver_wallet);
+
+    let denom = src_chain_driver.denom_at(TransferDenom, PhantomData::<Index<0>>);
+
+    let src_chain = src_chain_driver.chain();
+    let dst_chain = dst_chain_driver.chain();
+
+    let initial_balance_sender = src_chain.query_balance(sender_address, denom).await?;
+
+    let transfer_amount_1 = src_chain_driver
+        .random_amount(1000, &initial_balance_sender)
+        .await;
+
+    let start_height = src_chain.query_chain_height().await?;
+
+    let packet_1 = src_chain
+        .ibc_transfer_token(
+            src_channel_id,
+            src_port_id,
+            sender_wallet,
+            receiver_address,
+            &transfer_amount_1,
+            &src_chain.default_memo(),
+        )
+        .await?;
+
+    let sequence_1 = SrcChain::packet_sequence(&packet_1);
+
+    let src_chain_status = src_chain.query_chain_status().await?;
+
+    let _ = relay
+        .relay_receive_packet(SrcChain::chain_status_height(&src_chain_status), &packet_1)
+        .await?;
+
+    let expected_sender_balance =
+        SrcChain::subtract_amount(&initial_balance_sender, &transfer_amount_1)?;
+
+    let current_sender_balance = src_chain.query_balance(sender_address, denom).await?;
+
+    assert_eq!(expected_sender_balance, current_sender_balance);
+
+    // Assert only the ack is pending for packet with sequence 1
+    {
+        let is_received = dst_chain
+            .query_packet_is_received(dst_port_id, dst_channel_id, &sequence_1)
+            .await?;
+
+        assert!(is_received);
+
+        let is_cleared = src_chain
+            .query_packet_is_cleared(src_port_id, src_channel_id, &sequence_1)
+            .await?;
+
+        assert!(!is_cleared);
+    }
+
+    let transfer_amount_2 = src_chain_driver
+        .random_amount(1000, &current_sender_balance)
+        .await;
+
+    let packet_2 = src_chain
+        .ibc_transfer_token(
+            src_channel_id,
+            src_port_id,
+            sender_wallet,
+            receiver_address,
+            &transfer_amount_2,
+            &src_chain.default_memo(),
+        )
+        .await?;
+
+    let sequence_2 = SrcChain::packet_sequence(&packet_2);
+
+    let expected_balance_sender =
+        SrcChain::subtract_amount(&current_sender_balance, &transfer_amount_2)?;
+
+    let current_sender_balance = src_chain.query_balance(sender_address, denom).await?;
+
+    assert_eq!(expected_balance_sender, current_sender_balance);
+
+    // Assert both recv and ack are pending for packet with sequence 2
+    {
+        let is_received = dst_chain
+            .query_packet_is_received(dst_port_id, dst_channel_id, &sequence_2)
+            .await?;
+
+        assert!(!is_received);
+
+        let is_cleared = src_chain
+            .query_packet_is_cleared(src_port_id, src_channel_id, &sequence_2)
+            .await?;
+
+        assert!(!is_cleared);
+    }
+
+    let end_height = src_chain.query_chain_height().await?;
+
+    relay
+        .auto_relay_with_heights(SourceTarget, &start_height, Some(&end_height))
+        .await?;
+
+    // Assert both recv and ack have been cleared for packet with sequence 1 and 2
+    {
+        let is_received = dst_chain
+            .query_packet_is_received(dst_port_id, dst_channel_id, &sequence_1)
+            .await?;
+
+        assert!(is_received);
+
+        let is_cleared = src_chain
+            .query_packet_is_cleared(src_port_id, src_channel_id, &sequence_1)
+            .await?;
+
+        assert!(is_cleared);
+
+        let is_received = dst_chain
+            .query_packet_is_received(dst_port_id, dst_channel_id, &sequence_2)
+            .await?;
+
+        assert!(is_received);
+
+        let is_cleared = src_chain
+            .query_packet_is_cleared(src_port_id, src_channel_id, &sequence_2)
+            .await?;
+
+        assert!(is_cleared);
+    }
+
+    Ok(())
 }
