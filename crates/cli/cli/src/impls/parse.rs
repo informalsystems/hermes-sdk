2eaf52f7750e6ee3188d74c06e14770ad7c58b6d
Soares Chen <soares.chen@maybevoid.com>
2025-03-03 13:57:23 +0000

    Do not ignore errors in ParseInitCosmosChannelOptions

--- crates/cli/cli/src/impls/parse.rs
+++ crates/cli/cli/src/impls/parse.rs
@@ -4,20 +4,27 @@ use cgp::prelude::*;
 use hermes_cli_components::traits::parse::{ArgParser, ArgParserComponent};
 use hermes_cosmos_chain_components::types::channel::CosmosInitChannelOptions;
 use ibc::core::channel::types::channel::Order;
+use ibc::core::channel::types::error::ChannelError;
 use ibc::core::channel::types::Version;
+use ibc::core::host::types::error::IdentifierError;
 use ibc::core::host::types::identifiers::ConnectionId;
 
-const DEFAULT_VERSION: &str = "ics20-1";
+#[cgp_auto_getter]
+pub trait HasCosmosCreateChannelFields {
+    fn target_connection_id(&self) -> &String;
+
+    fn version(&self) -> &String;
+
+    fn ordering(&self) -> &String;
+}
 
 pub struct ParseInitCosmosChannelOptions;
 
 #[cgp_provider(ArgParserComponent)]
 impl<App, Args, Tag> ArgParser<App, Args, Tag> for ParseInitCosmosChannelOptions
 where
-    App: HasAsyncErrorType,
-    Args: HasField<symbol!("target_connection_id"), Value = String>
-        + HasField<symbol!("version"), Value = String>
-        + HasField<symbol!("ordering"), Value = String>,
+    App: CanRaiseAsyncError<IdentifierError> + CanRaiseAsyncError<ChannelError>,
+    Args: HasCosmosCreateChannelFields,
 {
     type Parsed = CosmosInitChannelOptions;
 
@@ -26,29 +33,17 @@ where
         args: &Args,
         _tag: PhantomData<Tag>,
     ) -> Result<Self::Parsed, App::Error> {
-        let connection_hops = if let Ok(conn_id) = args
-            .get_field(PhantomData::<symbol!("target_connection_id")>)
-            .parse::<ConnectionId>()
-        {
-            vec![conn_id]
-        } else {
-            Default::default()
-        };
+        let connection_id: ConnectionId = args
+            .target_connection_id()
+            .parse()
+            .map_err(App::raise_error)?;
 
-        let ordering =
-            if let Ok(ordering) = args.get_field(PhantomData::<symbol!("ordering")>).parse() {
-                ordering
-            } else {
-                Order::Unordered
-            };
+        let ordering: Order = args.ordering().parse().map_err(App::raise_error)?;
 
-        let channel_version = match args.get_field(PhantomData::<symbol!("version")>).parse() {
-            Ok(version) => version,
-            Err(_) => Version::new(DEFAULT_VERSION.to_string()),
-        };
+        let channel_version: Version = args.version().clone().into();
 
         Ok(CosmosInitChannelOptions {
-            connection_hops,
+            connection_hops: vec![connection_id],
             ordering,
             channel_version,
         })
