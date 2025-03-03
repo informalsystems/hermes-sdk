8c3d1ac9cfa88e3c385dc27e6f47cc9988f1509c
Soares Chen <soares.chen@maybevoid.com>
2025-03-03 11:49:39 +0000

    Use #[trait_alias] to simplify trait alias definitions

--- crates/relayer/relayer-components/src/relay/traits/chains/error.rs
+++ crates/relayer/relayer-components/src/relay/traits/chains/error.rs
@@ -1,18 +1,13 @@
 use cgp::core::error::ErrorOf;
+use cgp::core::macros::trait_alias;
 use cgp::prelude::*;
 
 use crate::relay::traits::chains::types::HasRelayChainTypes;
 
+#[trait_alias]
 pub trait CanRaiseRelayChainErrors:
     HasRelayChainTypes
     + CanRaiseAsyncError<ErrorOf<Self::SrcChain>>
     + CanRaiseAsyncError<ErrorOf<Self::DstChain>>
-{
-}
-
-impl<Relay> CanRaiseRelayChainErrors for Relay where
-    Relay: HasRelayChainTypes
-        + CanRaiseAsyncError<ErrorOf<Self::SrcChain>>
-        + CanRaiseAsyncError<ErrorOf<Self::DstChain>>
 {
 }
