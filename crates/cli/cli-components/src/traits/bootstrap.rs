961daddcc360155a3cf027cd419e09fefe14d9ee
Soares Chen <soares.chen@maybevoid.com>
2025-03-05 15:01:53 +0000

    Implement BootstrapTypeProvider for UseDelegate

--- crates/cli/cli-components/src/traits/bootstrap.rs
+++ crates/cli/cli-components/src/traits/bootstrap.rs
@@ -1,9 +1,9 @@
-use cgp::core::component::WithProvider;
+use cgp::core::component::{UseDelegate, WithProvider};
 use cgp::core::types::ProvideType;
 use cgp::prelude::*;
 
 #[cgp_type]
-pub trait HasBootstrapType<Tag>: Async {
+pub trait HasBootstrapType<Tag> {
     type Bootstrap: Async;
 }
 
@@ -14,4 +14,14 @@ pub trait HasBootstrapType<Tag>: Async {
 #[async_trait]
 pub trait CanLoadBootstrap<Tag, Args: Async>: HasBootstrapType<Tag> + HasAsyncErrorType {
     async fn load_bootstrap(&self, args: &Args) -> Result<Self::Bootstrap, Self::Error>;
+}
+
+#[cgp_provider(BootstrapTypeProviderComponent)]
+impl<Context, Tag, Components, Delegate> BootstrapTypeProvider<Context, Tag>
+    for UseDelegate<Components>
+where
+    Components: DelegateComponent<Tag, Delegate = Delegate>,
+    Delegate: BootstrapTypeProvider<Context, Tag>,
+{
+    type Bootstrap = Delegate::Bootstrap;
 }
