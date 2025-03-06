7f0d47b6ed51f5be68b19883087c4d0daa4361d2
Soares Chen <soares.chen@maybevoid.com>
2025-03-06 10:35:30 +0000

    Implement UseDelegate for BootstrapLoader

--- crates/cli/cli-components/src/traits/bootstrap.rs
+++ crates/cli/cli-components/src/traits/bootstrap.rs
@@ -25,3 +25,17 @@ where
 {
     type Bootstrap = Delegate::Bootstrap;
 }
+
+#[cgp_provider(BootstrapLoaderComponent)]
+impl<App, Tag, Args, Components, Delegate> BootstrapLoader<App, Tag, Args>
+    for UseDelegate<Components>
+where
+    App: HasBootstrapType<Tag> + HasAsyncErrorType,
+    Components: DelegateComponent<Tag, Delegate = Delegate>,
+    Delegate: BootstrapLoader<App, Tag, Args>,
+    Args: Async,
+{
+    async fn load_bootstrap(app: &App, args: &Args) -> Result<App::Bootstrap, App::Error> {
+        Delegate::load_bootstrap(app, args).await
+    }
+}
