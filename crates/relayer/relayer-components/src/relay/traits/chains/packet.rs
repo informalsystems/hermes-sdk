3089a08481da87425a161153f86aaad6cde46e02
Soares Chen <soares.chen@maybevoid.com>
2025-03-11 11:58:58 +0100

    Use `#[trait_alias]` to simplify some aliases for abstract relay types (#570)
    * Use #[trait_alias] to simplify trait alias definitions
    
    * New trait alias now works with HasBiRelayTypes
    
    * Use back main branch

--- crates/relayer/relayer-components/src/relay/traits/chains/packet.rs
+++ crates/relayer/relayer-components/src/relay/traits/chains/packet.rs
@@ -1,3 +1,4 @@
+use cgp::core::macros::trait_alias;
 use cgp::core::Async;
 use hermes_chain_components::traits::types::packet::HasOutgoingPacketType;
 
@@ -6,15 +7,9 @@ use crate::relay::traits::chains::{DstChainOf, HasRelayChains, SrcChainOf};
 pub type PacketOf<Relay> =
     <SrcChainOf<Relay> as HasOutgoingPacketType<DstChainOf<Relay>>>::OutgoingPacket;
 
+#[trait_alias]
 pub trait HasRelayPacketType:
     HasRelayChains<SrcChain: HasOutgoingPacketType<Self::DstChain, OutgoingPacket = Self::Packet>>
 {
     type Packet: Async;
-}
-
-impl<Relay> HasRelayPacketType for Relay
-where
-    Relay: HasRelayChains,
-{
-    type Packet = PacketOf<Relay>;
 }
