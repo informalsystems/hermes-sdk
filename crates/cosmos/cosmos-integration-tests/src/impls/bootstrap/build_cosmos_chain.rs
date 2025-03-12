5e54baea469aad6afd0a0c2864bf2122c3fa6ff4
Soares Chen <soares.chen@maybevoid.com>
2025-03-12 20:06:44 +0000

    Warn if GRPC service is still not started after max retries

--- crates/cosmos/cosmos-integration-tests/src/impls/bootstrap/build_cosmos_chain.rs
+++ crates/cosmos/cosmos-integration-tests/src/impls/bootstrap/build_cosmos_chain.rs
@@ -7,7 +7,7 @@ use hermes_cosmos_test_components::chain::types::wallet::CosmosTestWallet;
 use hermes_error::types::HermesError;
 use hermes_logging_components::traits::has_logger::HasLogger;
 use hermes_logging_components::traits::logger::CanLog;
-use hermes_logging_components::types::level::{LevelDebug, LevelTrace};
+use hermes_logging_components::types::level::{LevelDebug, LevelTrace, LevelWarn};
 use hermes_relayer_components::chain::traits::queries::chain_status::CanQueryChainStatus;
 use hermes_relayer_components::chain::traits::types::chain_id::HasChainId;
 use hermes_relayer_components::chain::traits::types::poll_interval::HasPollInterval;
@@ -21,7 +21,7 @@ use crate::traits::bootstrap::build_chain::{
 use crate::traits::bootstrap::cosmos_builder::HasCosmosBuilder;
 use crate::traits::bootstrap::relayer_chain_config::CanBuildRelayerChainConfig;
 
-const RETRY_COUNT: u64 = 40;
+const RETRY_COUNT: u64 = 100;
 
 #[cgp_new_provider(ChainBuilderWithNodeConfigComponent)]
 impl<Bootstrap> ChainBuilderWithNodeConfig<Bootstrap> for BuildCosmosChainWithNodeConfig
@@ -38,7 +38,7 @@ where
         + CanQueryGrpcServiceStatus
         + HasPollInterval
         + HasChainId
-        + HasLogger<Logger: CanLog<LevelDebug> + CanLog<LevelTrace>>,
+        + HasLogger<Logger: CanLog<LevelWarn> + CanLog<LevelDebug> + CanLog<LevelTrace>>,
 {
     async fn build_chain_with_node_config(
         bootstrap: &Bootstrap,
@@ -68,7 +68,7 @@ where
             .await;
 
         // Wait for both RPC and gRPC servers to start before resuming bootstrapping
-        for _ in 0..RETRY_COUNT {
+        for i in 0..RETRY_COUNT {
             let rpc_server_is_ready = chain.query_chain_status().await.is_ok();
 
             let grpc_server_is_ready = chain
@@ -77,22 +77,34 @@ where
                 .unwrap_or(false);
 
             if rpc_server_is_ready && grpc_server_is_ready {
+                chain
+                    .logger()
+                    .log(
+                        &format!(
+                            "RPC and GRPC services of chain `{}`  are now ready",
+                            chain.chain_id()
+                        ),
+                        &LevelDebug,
+                    )
+                    .await;
+
                 break;
+            } else if i + 1 == RETRY_COUNT {
+                chain
+                    .logger()
+                    .log(
+                        &format!(
+                            "RPC and GRPC services of chain `{}`  are still not ready after {} tries. Will continue with possible failure",
+                            chain.chain_id(),
+                            RETRY_COUNT,
+                        ),
+                        &LevelWarn,
+                    )
+                    .await;
             }
 
             bootstrap.runtime().sleep(chain.poll_interval()).await;
         }
-
-        chain
-            .logger()
-            .log(
-                &format!(
-                    "chain `{}` RPC and GRPC services to are now ready`",
-                    chain.chain_id()
-                ),
-                &LevelDebug,
-            )
-            .await;
 
         Ok(chain)
     }
