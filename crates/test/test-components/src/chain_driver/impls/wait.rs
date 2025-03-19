2fe461d5b93487b99213740b981149fb9c773be9
Soares Chen <soares.chen@maybevoid.com>
2025-03-19 13:54:24 +0000

    Log trace chain height query

--- crates/test/test-components/src/chain_driver/impls/wait.rs
+++ crates/test/test-components/src/chain_driver/impls/wait.rs
@@ -1,6 +1,10 @@
+use alloc::format;
 use core::time::Duration;
 
 use cgp::prelude::*;
+use hermes_logging_components::traits::has_logger::HasLogger;
+use hermes_logging_components::traits::logger::CanLog;
+use hermes_logging_components::types::level::LevelTrace;
 use hermes_relayer_components::chain::traits::queries::chain_status::CanQueryChainHeight;
 use hermes_relayer_components::chain::traits::types::height::HasHeightFields;
 use hermes_runtime_components::traits::runtime::HasRuntime;
@@ -16,14 +20,21 @@ impl<ChainDriver, Chain, const H: u64> ChainStartupWaiter<ChainDriver> for WaitC
 where
     ChainDriver:
         HasChain<Chain = Chain> + HasRuntime<Runtime: CanSleep> + CanRaiseAsyncError<&'static str>,
-    Chain: CanQueryChainHeight + HasHeightFields,
+    Chain: CanQueryChainHeight + HasHeightFields + HasLogger,
+    Chain::Logger: CanLog<LevelTrace>,
 {
     async fn wait_chain_startup(chain_driver: &ChainDriver) -> Result<(), ChainDriver::Error> {
         let runtime = chain_driver.runtime();
         let chain = chain_driver.chain();
+        let logger = chain.logger();
 
         for _ in 0..30 {
-            if let Ok(height) = chain.query_chain_height().await {
+            let res = chain.query_chain_height().await;
+            logger
+                .log(&format!("query chain height result: {res:?}"), &LevelTrace)
+                .await;
+
+            if let Ok(height) = res {
                 if Chain::revision_height(&height) >= H {
                     return Ok(());
                 }
