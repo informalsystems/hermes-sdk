6c632d0e08adbd9be9bbe3e47a8c934754b90320
Soares Chen <soares.chen@maybevoid.com>
2025-03-24 17:13:01 +0000

    Implement UseDelegate for IbcTransferredAmountConverter

--- crates/test/test-components/src/chain/traits/transfer/amount.rs
+++ crates/test/test-components/src/chain/traits/transfer/amount.rs
@@ -1,13 +1,14 @@
 use core::marker::PhantomData;
 
+use cgp::core::component::UseDelegate;
 use cgp::prelude::*;
 use hermes_relayer_components::chain::traits::types::ibc::HasIbcChainTypes;
 
 use crate::chain::traits::types::amount::HasAmountType;
 
 #[cgp_component {
-  provider: IbcTransferredAmountConverter,
-  context: Chain,
+    provider: IbcTransferredAmountConverter,
+    context: Chain,
 }]
 pub trait CanConvertIbcTransferredAmount<Counterparty>:
     HasAmountType + HasIbcChainTypes<Counterparty> + HasAsyncErrorType
@@ -26,4 +27,40 @@ where
         counterparty_amount: &Counterparty::Amount,
         denom: &Self::Denom,
     ) -> Self::Amount;
+}
+
+#[cgp_provider(IbcTransferredAmountConverterComponent)]
+impl<Chain, Counterparty, Components> IbcTransferredAmountConverter<Chain, Counterparty>
+    for UseDelegate<Components>
+where
+    Chain: HasAmountType + HasIbcChainTypes<Counterparty> + HasAsyncErrorType,
+    Counterparty: HasAmountType,
+    Components: DelegateComponent<Counterparty>,
+    Components::Delegate: IbcTransferredAmountConverter<Chain, Counterparty>,
+{
+    fn ibc_transfer_amount_from(
+        counterparty: PhantomData<Counterparty>,
+        counterparty_amount: &Counterparty::Amount,
+        channel_id: &Chain::ChannelId,
+        port_id: &Chain::PortId,
+    ) -> Result<Chain::Amount, Chain::Error> {
+        Components::Delegate::ibc_transfer_amount_from(
+            counterparty,
+            counterparty_amount,
+            channel_id,
+            port_id,
+        )
+    }
+
+    fn transmute_counterparty_amount(
+        counterparty: PhantomData<Counterparty>,
+        counterparty_amount: &Counterparty::Amount,
+        denom: &Chain::Denom,
+    ) -> Chain::Amount {
+        Components::Delegate::transmute_counterparty_amount(
+            counterparty,
+            counterparty_amount,
+            denom,
+        )
+    }
 }
